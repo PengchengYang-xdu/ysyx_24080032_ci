1a8d2b3a63a9fe7a13cd7382a395f75201f1cb9d
tracer-ysyx <tracer@ysyx.org>
2024-08-12 20:52:14 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:52:14 up 2 days, 20:55,  1 user,  load average: 0.59, 0.40, 0.41

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -219,6 +219,7 @@ word_t eval(int p, int q, bool *success) {
     if(!*success) return 0;
     word_t val2 = eval(op + 1, q, success);
     if(!*success) return 0;
+    printf("op = %d", op);
     printf("val1 = %u, val2 = %u\n", val1, val2);
     printf("tokens[op].type = %c\n", tokens[op].type);
     switch (tokens[op].type) {
