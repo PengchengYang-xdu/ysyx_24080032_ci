c7274f3218a928662d525b4acbf6c38ef126165d
tracer-ysyx <tracer@ysyx.org>
2024-08-19 21:58:08 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:08 up 1 day,  5:44,  1 user,  load average: 0.97, 1.10, 1.07

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -24,12 +24,6 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  int reg_num = ARRLEN(regs);
-  int i;
-
-  for (i = 0; i < reg_num; i++) {
-    printf("%-8s%-#20lx%-20ld\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
-  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
