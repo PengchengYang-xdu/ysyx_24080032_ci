65c3497a464e09108a3cac22ea88afa44aab19c1
tracer-ysyx <tracer@ysyx.org>
2024-08-20 17:04:25 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:04:25 up 1 day, 18:10,  1 user,  load average: 1.44, 1.07, 0.76

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -67,6 +67,7 @@ void wp_set(char *expr, word_t res) {
   WP* wp = new_wp();
   printf("crate success\n");
   strcpy(wp->expr, expr);
+  printf("strcpy success\n");
   wp->old = res;
   printf("Watchpoint %d: %s\n", wp->NO, expr);
 }
