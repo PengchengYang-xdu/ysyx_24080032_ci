d0f4eac7dae2ce641884f4a7d9e95066dd65817f
tracer-ysyx <tracer@ysyx.org>
2024-08-20 17:56:33 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:33 up 1 day, 19:02,  1 user,  load average: 1.04, 0.83, 0.84

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -126,7 +126,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        printf("rules.token_type = %c\n", rules[i].token_type);
+        printf("rules.token_type = %d\n", rules[i].token_type);
 
         switch (rules[i].token_type) {
           case TK_NUM :
