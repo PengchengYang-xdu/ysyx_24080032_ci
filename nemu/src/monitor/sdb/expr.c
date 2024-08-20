a9a573626499c9ef8430e7c03cf33f48827a62dd
tracer-ysyx <tracer@ysyx.org>
2024-08-20 17:29:50 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:29:50 up 1 day, 18:36,  1 user,  load average: 0.84, 0.88, 0.88

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -184,6 +184,7 @@ int find_major(int p, int q){
   int del = 0;
   int last_low_p = 0;
   for(int i = p; i <= q; i++){
+    printf("p = %d, q = %d\n", p, q);
     printf("tokens[i].type = %d\n", tokens[i].type);
     if(tokens[i].type == '(')
       del++;
