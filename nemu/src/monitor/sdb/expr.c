1aef6eca80eb87486aeff48bd227c98d46009785
tracer-ysyx <tracer@ysyx.org>
2024-08-20 17:46:23 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:23 up 1 day, 18:52,  1 user,  load average: 1.24, 0.93, 0.89

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -137,6 +137,7 @@ static bool make_token(char *e) {
             if(nr_token == 0 || (tokens[nr_token - 1].type != ')' && tokens[nr_token - 1].type != TK_NUM && tokens[nr_token - 1].type != TK_REG)){
               printf("yiyuan\n");
               printf("rules[i].token_type = %c\n", rules[i].token_type);
+              printf("nr_token = %d\n", nr_token);
               switch (rules[i].token_type) {
                 case '*' : tokens[nr_token].type = TK_DEREF;
                 case '+' : tokens[nr_token].type = TK_POS;
