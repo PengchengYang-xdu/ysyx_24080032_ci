bd826c805d9d17d043b1d04e920876ee72b72442
tracer-ysyx <tracer@ysyx.org>
2024-09-19 15:09:44 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:09:44 up 5 days, 17:21,  1 user,  load average: 0.83, 0.67, 0.61

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -89,15 +89,22 @@ void *memcpy(void *out, const void *in, size_t n) {
 
 int memcmp(const void *s1, const void *s2, size_t n) {
   // panic("Not implemented");
-  const unsigned char *p1 = s1, *p2 = s2;
-  while (n--) {
-    if (*p1 != *p2) {
-      return *p1 - *p2;
-    }
-    p1++;
-    p2++;
-  }
-  return 0;
+  const unsigned char *p1, *p2;
+  int res = 0;
+  for(p1 = s1, p2 = s2; 0 < n; p1++, p2++, n--)
+    if((res = *p1 - *p2) != 0)
+      break;
+  return res;
+
+  // const unsigned char *p1 = s1, *p2 = s2;
+  // while (n--) {
+  //   if (*p1 != *p2) {
+  //     return *p1 - *p2;
+  //   }
+  //   p1++;
+  //   p2++;
+  // }
+  // return 0;
 }
 
 #endif
