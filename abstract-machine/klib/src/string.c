1248c904ee92157166221e06732c060bd54bb1cf
tracer-ysyx <tracer@ysyx.org>
2024-09-19 15:02:58 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:58 up 5 days, 17:14,  1 user,  load average: 0.83, 0.89, 0.65

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -56,9 +56,9 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
   // panic("Not implemented");
-  unsigned char *p = s;
+  char *p = s;
   while (n--) {
-    *p++ = (unsigned char)c;
+    *p++ = (char)c;
   }
   return s;
 }
