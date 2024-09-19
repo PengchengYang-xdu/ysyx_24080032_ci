98ecac2a4c526ab416a205d8ca5721db911276ef
tracer-ysyx <tracer@ysyx.org>
2024-09-19 22:21:12 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:21:12 up 5 days, 20:45,  1 user,  load average: 0.87, 0.93, 0.81

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -17,6 +17,7 @@
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
+#include <utils.h>
 
 #define R(i) gpr(i)
 #define Mr vaddr_read
@@ -131,5 +132,6 @@ static int decode_exec(Decode *s) {
 
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
+  IFDEF(CONFIG_ITRACE, itrace_init(s->pc, s->isa.inst.val));
   return decode_exec(s);
 }
