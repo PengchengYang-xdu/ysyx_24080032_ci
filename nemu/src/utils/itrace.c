97cfe079a0ae62624a8b6ea44e58e581f13f7439
tracer-ysyx <tracer@ysyx.org>
2024-09-20 11:17:04 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:17:04 up 5 days, 21:21,  1 user,  load average: 2.09, 1.45, 1.09

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -34,8 +34,12 @@ void display_inst() {
     p += sprintf(buf, "%s" FMT_WORD ": %08x ", ((start + 1) % MAX_IRINGBUF == end) ? " --> " : "     ", iringbuf[start].pc, iringbuf[start].inst);
     disassemble(p, buf+sizeof(buf)-p, iringbuf[start].pc, (uint8_t *)&iringbuf[start].inst, 4);
 
-    if((start+1)%MAX_IRINGBUF==end && is_exit_status_bad()) printf(ANSI_FG_RED);// abort state is RED
-    else if((start+1)%MAX_IRINGBUF==end && !is_exit_status_bad()) printf(ANSI_FG_GREEN);// end state is GREEN
+    if((start + 1) % MAX_IRINGBUF == end && is_exit_status_bad()){
+        if(is_exit_status_bad())
+            printf(ANSI_FG_RED);
+        else
+            printf(ANSI_FG_GREEN);
+    }
     puts(buf);
   }
   puts(ANSI_NONE);
