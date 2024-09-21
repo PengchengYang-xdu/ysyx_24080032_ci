ef5810f007833d9b9191fd6a8625edffed6e17dd
tracer-ysyx <tracer@ysyx.org>
2024-09-21 12:36:17 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:36:17 up 36 min,  1 user,  load average: 1.20, 0.79, 0.72

--- nemu/src/utils/mtrace.c
+++ nemu/src/utils/mtrace.c
@@ -2,11 +2,9 @@
 #include <utils.h>
 
 void display_pread(paddr_t addr, int len) {
-  if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
-    printf("pread at " FMT_PADDR " len = %d\n", addr, len);
+  printf("pread at " FMT_PADDR " len = %d\n", addr, len);
 }
 
 void display_pwrite(paddr_t addr, int len, word_t data) {
-  if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
-    printf("pwrite at " FMT_PADDR " len = %d  data = " FMT_WORD "\n", addr, len, data);
+  printf("pwrite at " FMT_PADDR " len = %d  data = " FMT_WORD "\n", addr, len, data);
 }
