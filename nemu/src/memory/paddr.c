d1005f6767bfbf02e01b18ae4dbf3f06f29e0fe9
tracer-ysyx <tracer@ysyx.org>
2024-09-21 12:18:12 +0800

    > compile NEMU ysyx_100006503 杨鹏城 Linux ypc 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:18:12 up 18 min,  1 user,  load average: 1.10, 0.80, 0.57

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,8 +51,10 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
-    IFDEF(CONFIG_MTRACE, display_pread(addr, len));
+  IFDEF(CONFIG_MTRACE, 
+    (if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
+      display_pread(addr, len))
+  );
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -60,8 +62,10 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
-    IFDEF(CONFIG_MTRACE, display_pwrite(addr, len, data));
+  IFDEF(CONFIG_MTRACE, 
+    (if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END)
+      display_pwrite(addr, len, data))
+  );
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
