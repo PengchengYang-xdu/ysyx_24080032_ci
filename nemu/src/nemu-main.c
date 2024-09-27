b283860a4f21d70bc44334f7c3967968a4cee5b8
tracer-ysyx <tracer@ysyx.org>
2024-09-27 12:03:39 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:03:39 up 3 days, 14:41,  1 user,  load average: 2.33, 1.88, 1.75

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -21,6 +21,9 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
+    printf("Argument %d: %s\n", i, argv[i]);
+  }
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
