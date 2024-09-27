29baca26e19d6d913586988f41f9f2d380d6a914
tracer-ysyx <tracer@ysyx.org>
2024-09-27 15:04:06 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:04:06 up 3 days, 17:41,  1 user,  load average: 1.68, 1.53, 1.51

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,11 +19,7 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  void *nemu_buf = (void *)guest_to_host(addr);
-  if(direction == DIFFTEST_TO_REF)  //dut -> ref (buf -> addr(nemu_buf))
-    memcpy(nemu_buf , buf , n);
-  else                              //ref -> dut (addr(nemu_buf) -> buf)
-    memcpy(buf , nemu_buf, n);
+  assert(0);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
