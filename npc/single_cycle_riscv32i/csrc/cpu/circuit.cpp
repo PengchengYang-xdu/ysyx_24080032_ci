18c385e7b3df74a50d32412d40d16fdba8ec755b
tracer-ysyx <tracer@ysyx.org>
2024-09-27 19:57:10 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:10 up 3 days, 22:34,  1 user,  load average: 1.70, 1.65, 1.63

--- npc/single_cycle_riscv32i/csrc/cpu/circuit.cpp
+++ npc/single_cycle_riscv32i/csrc/cpu/circuit.cpp
@@ -9,6 +9,8 @@ deigned by ypc
 #include <debug.h>
 
 Vysyx_24080032_riscv32i *top = init_top();
+static uint8_t opcode;
+static uint8_t rd;
 uint64_t g_nr_guest_inst = 0;
 void difftest_step();
 
@@ -50,23 +52,24 @@ void assert_fail_msg() {
 
 static void trace_and_difftest(){
 
-	/* DiffTest */
 	#ifdef NPCCONFIG_DIFFTEST
 	difftest_step();
 	#endif
 
-	/* watchpoint check */
 	#ifdef NPCCONFIG_WATCHPOINT
 	wp_difftest();
 	#endif
 
-	#ifdef CONFIG_FTRACE
-	/* trace(2):function trace*/
-	extern char * elf_file;
-	opcode = BITS(inst, 6, 0);
-	if(opcode == JAL || opcode == JALR){
-		ftrace_check(opcode ,prev_pc, dnpc, inst);
-	}
+	#ifdef NPCCONFIG_FTRACE
+	opcode = BITS(top->rootp -> ysyx_24080032_riscv32i__DOT__Instr, 6, 0);
+	rd = BITS(top->rootp -> ysyx_24080032_riscv32i__DOT__Instr, 11, 7);
+	if(opcode == JAL && rd == 0b00001)
+		display_call_func(ysyx_24080032_riscv32i__DOT__PC, ysyx_24080032_riscv32i__DOT__NextPC);
+	else if(opcode == JALR)
+		if(rd == 0b00001)
+			display_call_func(ysyx_24080032_riscv32i__DOT__PC, ysyx_24080032_riscv32i__DOT__NextPC);
+		else if(rd == 0b00000 && BITS(top->rootp -> ysyx_24080032_riscv32i__DOT__Instr, 19, 15) == gpr[1])
+			display_ret_func(ysyx_24080032_riscv32i__DOT__PC);
 	#endif
 
 }
@@ -103,35 +106,4 @@ extern "C" void npc_trap(){
 	
 	statistic();
 	exit(0);
-}
-
-static void trace_and_difftest(){
-	/* DiffTest */
-	difftest_step();
-
-	/* watchpoint check */
-	extern int check_w();
-  	int no = check_w();
-  	if(no != 0){
-    	printf("NO.%d watchpoint has been trigger\n",no);
-		return;
-  	}
-
-	/* trace(1):instruction trace */
-	char disasm_buf[128] = {0};
-	record_inst_trace(disasm_buf,(uint8_t *)&inst);
-	//print to stdout
-	if(g_print_step) puts(disasm_buf);
-	//print to log file
-	log_write("%s\n", disasm_buf);
-
-	#ifdef CONFIG_FTRACE
-	/* trace(2):function trace*/
-	extern char * elf_file;
-	opcode = BITS(inst, 6, 0);
-	if(opcode == JAL || opcode == JALR){
-		ftrace_check(opcode ,prev_pc, dnpc, inst);
-	}
-	#endif
-
 }
