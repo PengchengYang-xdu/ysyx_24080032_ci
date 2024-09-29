4df4f6d2cbe03c57e5e06338d95c6efade5185cb
tracer-ysyx <tracer@ysyx.org>
2024-09-29 18:32:28 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:32:28 up 5 days, 21:09,  1 user,  load average: 1.80, 1.58, 1.49

--- nemu/src/utils/filelist.mk
+++ nemu/src/utils/filelist.mk
@@ -13,6 +13,7 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
+CXXSRC = src/utils/disasm.cc
 ifneq ($(CONFIG_ITRACE)$(CONFIG_IQUEUE),)
 CXXSRC = src/utils/disasm.cc
 CXXFLAGS += $(shell llvm-config --cxxflags) -fPIE
