6586605a22db79190ac07b5b207d04625714357d
tracer-ysyx <tracer@ysyx.org>
2024-09-29 21:32:59 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:32:59 up  1:31,  1 user,  load average: 2.17, 2.12, 2.09

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -34,7 +34,6 @@ uint8_t* new_space(int size) {
 
 static void check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
-    IFDEF(CONFIG_ITRACE, display_inst());//print recent instructions when ABORT or END
     Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
   } else {
     Assert(addr <= map->high && addr >= map->low,
