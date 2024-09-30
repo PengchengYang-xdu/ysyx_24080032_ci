fd8102d9bdef7c9bea3141e78c2956fb308964d5
tracer-ysyx <tracer@ysyx.org>
2024-09-30 11:50:20 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:50:20 up 15:49,  1 user,  load average: 2.21, 1.90, 1.78

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -17,6 +17,7 @@
 #define __UTILS_H__
 
 #include <common.h>
+#include <device/map.h>
 
 // ----------- state -----------
 
@@ -91,3 +92,8 @@ void display_pwrite(paddr_t addr, int len, word_t data);
 void parse_elf(const char *elf_file);
 void display_call_func(word_t pc, word_t func_addr);
 void display_ret_func(word_t pc);
+
+// // ----------- dtrace -----------
+
+// void display_dread(paddr_t addr, int len, IOMap *map);
+// void display_dwrite(paddr_t addr, int len, word_t data, IOMap *map);
