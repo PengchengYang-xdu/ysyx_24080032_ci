0b639414c36f56146992b665dbb29f2ea6d72ae1
tracer-ysyx <tracer@ysyx.org>
2024-09-30 22:35:13 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:35:13 up 1 day,  2:33,  1 user,  load average: 1.40, 1.51, 1.52

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -29,10 +29,28 @@ int atoi(const char* nptr) {
   return x;
 }
 
+// 定义一个静态的内存池
+#define MEMORY_POOL_SIZE 1024 * 1024  // 1MB的内存池
+static char memory_pool[MEMORY_POOL_SIZE];
+static char *free_ptr = memory_pool;  // 指向下一个可用的内存位置
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  // 对齐分配的内存大小，以确保分配的内存地址是对齐的
+  size = (size + sizeof(size_t) - 1) & ~(sizeof(size_t) - 1);
+
+  // 检查是否还有足够的内存可分配
+  if (free_ptr + size > memory_pool + MEMORY_POOL_SIZE) {
+    return NULL;  // 如果内存不足，返回 NULL
+  }
+
+  // 返回当前的 free_ptr，然后将其向后移动 size 个字节
+  void *result = free_ptr;
+  free_ptr += size;
+
+  return result;
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif
