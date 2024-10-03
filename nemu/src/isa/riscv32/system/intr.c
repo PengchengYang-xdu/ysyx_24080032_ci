c98cc21dc78da5cc9f4caba17a50abb096a33b62
tracer-ysyx <tracer@ysyx.org>
2024-10-03 22:49:17 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:17 up 2 days, 37 min,  1 user,  load average: 1.85, 1.78, 1.58

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -23,7 +23,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /*i need to realize save the mepc mcause and mtvec*/
   if(NO == 0xb){
     csr(MCAUSE) = NO;
-    csr(MEPC) = epc;
+    csr(MEPC) = epc + 4;
     printf("epc = 0x%x\n", epc);
     return csr(MTVEC);
   }
