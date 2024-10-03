e95d868a8277fce6142b00d14557846cc9c4f0ef
tracer-ysyx <tracer@ysyx.org>
2024-10-03 12:54:41 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:54:41 up 1 day, 14:43,  1 user,  load average: 1.27, 1.27, 1.30

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -95,7 +95,7 @@ static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
-    printf("!!! difftest checkregs abort at pc = 0x%x !!!\n", pc);
+    printf(ANSI_FMT("!!! difftest checkregs abort at pc = 0x%x !!!\n", ANSI_FG_RED), pc);
     isa_reg_display();
   }
 }
