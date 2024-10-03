5a87a5df433bef75c9c8abeb716aa5fb1edd61b3
tracer-ysyx <tracer@ysyx.org>
2024-10-03 22:48:40 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:40 up 2 days, 37 min,  1 user,  load average: 1.97, 1.77, 1.57

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -22,9 +22,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   /*i need to realize save the mepc mcause and mtvec*/
   if(NO == 0xb){
+    csr(MCAUSE) = NO;
     csr(MEPC) = epc;
     printf("epc = 0x%x\n", epc);
-    csr(MCAUSE) = NO;
     return csr(MTVEC);
   }
   panic("irq is not zixian");
