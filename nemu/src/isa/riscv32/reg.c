e6d79593c5ad29ed8232dae123f6193f8fc50756
tracer-ysyx <tracer@ysyx.org>
2024-10-03 13:03:03 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:03:03 up 1 day, 14:51,  1 user,  load average: 1.74, 1.52, 1.41

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,6 +14,7 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include <cpu/difftest.h>
 #include "local-include/reg.h"
 
 const char *regs[] = {
@@ -23,15 +24,27 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-void isa_reg_display() {
+void isa_reg_display(CPU_state *ref) {
   int length = sizeof(regs) / sizeof(regs[0]);
-  for(int i = 0; i < length; i ++)
-    printf("reg %s ---> 0x%x\n", regs[i], cpu.gpr[i]);
+  for(int i = 0; i < length; i ++){
+    printf("reg %s ---> 0x%x", regs[i], cpu.gpr[i]);
+    #ifdef CONFIG_DIFFTEST
+    printf("ref-reg %s ---> 0x%x\n", regs[i], ref->gpr[i]);
+    #endif
+    printf("\n");
+  }
+
   printf("\n");
   printf("csr-mtvec   --->  0x%x\n",cpu.csr[MTVEC]);
   printf("csr-mepc    --->  0x%x\n",cpu.csr[MEPC]);
   printf("csr-mstatus --->  0x%x\n",cpu.csr[MSTATUS]);
   printf("csr-mcause  --->  0x%x\n",cpu.csr[MCAUSE]);
+  #ifdef CONFIG_DIFFTEST
+  printf("ref-csr-mtvec   --->  0x%x\n",ref->csr[MTVEC]);
+  printf("ref-csr-mepc    --->  0x%x\n",ref->csr[MEPC]);
+  printf("ref-csr-mstatus --->  0x%x\n",ref->csr[MSTATUS]);
+  printf("ref-csr-mcause  --->  0x%x\n",ref->csr[MCAUSE]);
+  #endif
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
