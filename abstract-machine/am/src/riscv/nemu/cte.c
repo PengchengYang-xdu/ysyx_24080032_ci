da1271bc3ff45f579e7e8392f4a69c21b460ebf3
tracer-ysyx <tracer@ysyx.org>
2024-10-04 16:30:35 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:35 up 2 days, 18:19,  1 user,  load average: 1.13, 1.13, 1.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -35,7 +35,10 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  Context *c = (Context *)kstack.end - 1;
+  c->mepc = (uintptr_t)entry;
+  c->mstatus = 0x1800;
+  return c;
 }
 
 void yield() {
