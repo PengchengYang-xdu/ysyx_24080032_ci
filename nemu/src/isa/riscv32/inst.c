eec9ab1eed029ddcf010c1d0dae74ecfa664cf02
tracer-ysyx <tracer@ysyx.org>
2024-10-04 17:10:11 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:11 up 2 days, 18:58,  1 user,  load average: 2.05, 1.89, 1.58

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -24,6 +24,14 @@
 #define Mr vaddr_read
 #define Mw vaddr_write
 
+#define MRET() { \
+  s->dnpc = CSR(MEPC); \
+  CSR(MSTATUS) &= ~(1<<3); \
+  CSR(MSTATUS) |= ((CSR(MSTATUS)&(1<<7))>>4); \
+  CSR(MSTATUS) |= (1<<7); \
+  CSR(MSTATUS) &= ~((1<<11)+(1<<12));\
+}
+
 enum {
   TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_R, TYPE_B,
   TYPE_N, // none
@@ -124,10 +132,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 010 ????? 01100 11", slt    , R, R(rd) = (sword_t)src1 < (sword_t)src2);
 
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, bool success; etrace(isa_reg_str2val("a7", &success), s->pc); s->dnpc = isa_raise_intr(isa_reg_str2val("a7", &success), s->pc));//
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , I, s->dnpc = CSR(MEPC); CSR(MSTATUS) &= ~(1<<3); \
-  CSR(MSTATUS) |= ((CSR(MSTATUS)&(1<<7))>>4); \
-  CSR(MSTATUS) |= (1<<7); \
-  CSR(MSTATUS) &= ~((1<<11)+(1<<12)));
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , I, MRET());
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm); CSR(imm) = src1);
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSR(imm); CSR(imm) |= src1);
 
