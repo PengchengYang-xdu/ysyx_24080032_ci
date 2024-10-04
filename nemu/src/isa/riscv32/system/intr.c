6fdb9e204a2d269f97b45322f51f152b6517273b
tracer-ysyx <tracer@ysyx.org>
2024-10-04 17:00:17 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:17 up 2 days, 18:48,  1 user,  load average: 1.59, 1.33, 1.31

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -21,6 +21,11 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    * Then return the address of the interrupt/exception vector.
    */
   /*i need to realize save the mepc mcause and mtvec*/
+  csr(MSTATUS) &= ~(1<<7);
+  csr(MSTATUS) |= ((csr(MSTATUS)&(1<<3))<<4);
+  csr(MSTATUS) &= ~(1<<3);
+  csr(MSTATUS) |= ((1<<11)+(1<<12));
+
   csr(MCAUSE) = NO;
   csr(MEPC) = epc;
   return csr(MTVEC);
