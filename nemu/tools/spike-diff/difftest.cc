8b19f29dc80ed508b9c9e5105c956f156c521a2c
tracer-ysyx <tracer@ysyx.org>
2024-10-12 16:24:05 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:05 up 1 day,  5:40,  1 user,  load average: 1.85, 1.88, 1.83

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -44,7 +44,7 @@ static debug_module_config_t difftest_dm_config = {
 struct diff_context_t {
   word_t gpr[MUXDEF(CONFIG_RVE, 16, 32)];
   word_t pc;
-  // word_t csr[4096];
+  word_t csr[4096];
 };
 
 static sim_t* s = NULL;
@@ -67,10 +67,10 @@ void sim_t::diff_get_regs(void* diff_context) {
   }
   ctx->pc = state->pc;
 
-  // ctx->csr[MEPC] = (state->mepc)->read();
-  // ctx->csr[MCAUSE] = (state->mcause)->read();
-  // ctx->csr[MSTATUS] = (state->mstatus)->read();
-  // ctx->csr[MTVEC] = (state->mtvec)->read();
+  ctx->csr[MEPC] = (state->mepc)->read();
+  ctx->csr[MCAUSE] = (state->mcause)->read();
+  ctx->csr[MSTATUS] = (state->mstatus)->read();
+  ctx->csr[MTVEC] = (state->mtvec)->read();
 }
 
 void sim_t::diff_set_regs(void* diff_context) {
@@ -80,10 +80,10 @@ void sim_t::diff_set_regs(void* diff_context) {
   }
   state->pc = ctx->pc;
 
-  // state->mepc->write(ctx->csr[MEPC]);
-  // state->mcause->write(ctx->csr[MCAUSE]);
-  // state->mstatus->write(ctx->csr[MSTATUS]);
-  // state->mtvec->write(ctx->csr[MTVEC]);
+  state->mepc->write(ctx->csr[MEPC]);
+  state->mcause->write(ctx->csr[MCAUSE]);
+  state->mstatus->write(ctx->csr[MSTATUS]);
+  state->mtvec->write(ctx->csr[MTVEC]);
 }
 
 void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
