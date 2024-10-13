08c3972de714451b01337efbd4a7ffe9ccdc2632
tracer-ysyx <tracer@ysyx.org>
2024-10-13 15:23:14 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.5.0-44-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 18 14:36:16 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:23:14 up 2 days,  4:40,  1 user,  load average: 1.53, 1.54, 1.45

--- npc/single_cycle_riscv32i_refactor/csrc/cpu/difftest/dut.cpp
+++ npc/single_cycle_riscv32i_refactor/csrc/cpu/difftest/dut.cpp
@@ -71,10 +71,10 @@ bool static checkregs(struct CPU_state *ref_r){
     if(ref_r -> gpr[i] != gpr[i])
       flag = false;
   }
-  // for(i = 0;i < 4;i++){
-  //   if(ref_r -> csr[i] != csr[i])
-  //     flag = false;
-  // }
+  for(i = 0;i < 4;i++){
+    if(ref_r -> csr[i] != csr[i])
+      flag = false;
+  }
   if(flag == false){
     printf("ref - pc = 0x%x\n",ref_r -> pc);
     printf("ref - pc = 0x%x\n",top->rootp -> ysyx_24080032_riscv32i__DOT__PC_if2id);
