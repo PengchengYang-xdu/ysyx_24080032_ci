9bafa8f7d9f9607757c7dcb99694284e068df9e3
tracer-ysyx <tracer@ysyx.org>
2024-11-10 17:01:29 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:29 up 1 day, 16:55,  1 user,  load average: 1.61, 1.64, 1.68

--- npc/single_cycle_riscv32i_refactor/csrc/cpu/wave.cpp
+++ npc/single_cycle_riscv32i_refactor/csrc/cpu/wave.cpp
@@ -10,15 +10,19 @@ vluint64_t main_time = 0;
 
 void init_wave(){
     Verilated::traceEverOn(true);
-	contextp = new VerilatedContext;	
+	contextp = new VerilatedContext;
 	tfp = new VerilatedVcdC();
 	top->trace(tfp, 5);
 	tfp->open("single_cycle_riscv32i.vcd");
 } 
 
 void dump_wave(){
-    tfp->dump(main_time);
-	main_time++;
+    // tfp->dump(main_time);
+	// main_time++;
+	// printf("dump wave now, main time = %ld\n", main_time);
+	tfp->dump(contextp -> time());
+	contextp -> timeInc(1);
+	printf("dump wave now, main time = %ld\n", contextp -> time());
 } 
 
 void close_wave(){
