0135f88554421783cc8798f2b653165bdb904af7
tracer-ysyx <tracer@ysyx.org>
2024-12-06 22:04:23 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:04:23 up 4 days, 51 min,  1 user,  load average: 1.25, 1.23, 1.19

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -9,6 +9,8 @@ AM_SRCS := riscv/ysyxsoc/start.S \
            platform/dummy/mpe.c \
 \
 		   riscv/ysyxsoc/bootloader.c \
+		   riscv/ysyxsoc/gpu.c \
+		   
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linkerysyxsoc.ld \
