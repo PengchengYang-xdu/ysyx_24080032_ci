5af88b73517746d64b86801d0c6a9ca1e80d9e8c
tracer-ysyx <tracer@ysyx.org>
2025-01-23 18:33:41 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:33:41 up 2 days, 10:47,  1 user,  load average: 1.39, 1.45, 1.35

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -44,6 +44,7 @@ uint8_t* guest_to_host(paddr_t paddr) {
         ptr = psram + paddr - PSRAM_BASE;
     return ptr;
 }
+
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
@@ -69,6 +70,14 @@ void init_mem() {
   Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
+
+
+
+
+
+
+
+
 word_t paddr_read(paddr_t addr, int len) {
 #ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return 0;}
@@ -87,6 +96,11 @@ word_t paddr_read(paddr_t addr, int len) {
   return 0;
 }
 
+
+
+
+
+
 void paddr_write(paddr_t addr, int len, word_t data) {
 #ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return;}
@@ -102,3 +116,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
+
+
+
+
