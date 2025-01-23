7c8ed1a6dac47553616fd9541950e3886123b405
tracer-ysyx <tracer@ysyx.org>
2025-01-23 12:40:55 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:40:55 up 2 days,  4:55,  1 user,  load average: 1.00, 1.02, 1.07

--- nemu/include/ysyxsoc.h
+++ nemu/include/ysyxsoc.h
@@ -15,4 +15,14 @@ bool in_sram(paddr_t addr);
 bool in_flash(paddr_t addr);
 bool in_sdram(paddr_t addr);
 bool in_psram(paddr_t addr);
+
+bool in_uart(paddr_t addr);
+bool in_clint(paddr_t addr);
+bool in_gpio(paddr_t addr);
+bool in_spi(paddr_t addr);
+bool in_ps2(paddr_t addr);
+bool in_vga(paddr_t addr);
+bool in_mmio(paddr_t addr);
+
+bool in_dev(paddr_t addr);
 #endif
