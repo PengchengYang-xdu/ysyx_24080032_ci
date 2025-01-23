279ed4e043b8675a5d128e0eb41d1b1e9e51da7d
tracer-ysyx <tracer@ysyx.org>
2025-01-23 17:05:22 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:22 up 2 days,  9:19,  1 user,  load average: 1.08, 1.11, 1.06

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -75,11 +75,11 @@ word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pread(addr, len));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
 
-  if (in_mrom(addr)) return pmem_read(addr, len);
-  if (in_sram(addr)) return pmem_read(addr, len);
-  if (in_flash(addr)) return pmem_read(addr, len);
-  if (in_sdram(addr)) return pmem_read(addr, len);
-  if (in_psram(addr)) return pmem_read(addr, len);
+//   if (in_mrom(addr)) return pmem_read(addr, len);
+//   if (in_sram(addr)) return pmem_read(addr, len);
+//   if (in_flash(addr)) return pmem_read(addr, len);
+//   if (in_sdram(addr)) return pmem_read(addr, len);
+//   if (in_psram(addr)) return pmem_read(addr, len);
 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -92,10 +92,10 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pwrite(addr, len, data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
 
-  if (in_sram(addr)) { pmem_write(addr, len, data); return; }
-  if (in_flash(addr)) { pmem_write(addr, len, data); return; }
-  if (in_sdram(addr)) { pmem_write(addr, len, data); return; }
-  if (in_psram(addr)) { pmem_write(addr, len, data); return; }
+//   if (in_sram(addr)) { pmem_write(addr, len, data); return; }
+//   if (in_flash(addr)) { pmem_write(addr, len, data); return; }
+//   if (in_sdram(addr)) { pmem_write(addr, len, data); return; }
+//   if (in_psram(addr)) { pmem_write(addr, len, data); return; }
 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
