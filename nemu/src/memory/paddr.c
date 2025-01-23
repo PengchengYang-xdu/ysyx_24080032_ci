cff9582cfa75c63a410e682949038d0939ad4ad1
tracer-ysyx <tracer@ysyx.org>
2025-01-23 17:40:45 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:40:45 up 2 days,  9:54,  1 user,  load average: 1.39, 1.35, 1.24

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -30,7 +30,7 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(paddr_t paddr) {
     uint8_t* ptr = NULL;
-    if(in_pmem(paddr))//change menu base to 0x20000000 and size to 0xfff, in_pmem === in_mrom
+    if(in_pmem(paddr))
         ptr = pmem + paddr - CONFIG_MBASE;
     else if(in_mrom(paddr))//write and read mrom
         ptr = mrom + paddr - MROM_BASE;
@@ -70,31 +70,36 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pread(addr, len));
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-
+#ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return 0;}
+
   if (in_mrom(addr)) return pmem_read(addr, len);
   if (in_sram(addr)) return pmem_read(addr, len);
   if (in_flash(addr)) return pmem_read(addr, len);
   if (in_sdram(addr)) return pmem_read(addr, len);
   if (in_psram(addr)) return pmem_read(addr, len);
+#endif
 
+
+  IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pread(addr, len));
+  if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pwrite(addr, len, data));
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-
+#ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return;}
+
   if (in_sram(addr)) { pmem_write(addr, len, data); return; }
   if (in_flash(addr)) { pmem_write(addr, len, data); return; }
   if (in_sdram(addr)) { pmem_write(addr, len, data); return; }
   if (in_psram(addr)) { pmem_write(addr, len, data); return; }
+#endif
 
+  IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pwrite(addr, len, data));
+  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
