67baac72cb46b2e86115e85ff444213262ea2a83
tracer-ysyx <tracer@ysyx.org>
2025-01-23 18:02:13 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:13 up 2 days, 10:16,  1 user,  load average: 1.36, 1.34, 1.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -70,7 +70,6 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-// #ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return 0;}
 
   if (in_mrom(addr)) return pmem_read(addr, len);
@@ -78,8 +77,6 @@ word_t paddr_read(paddr_t addr, int len) {
   if (in_flash(addr)) return pmem_read(addr, len);
   if (in_sdram(addr)) return pmem_read(addr, len);
   if (in_psram(addr)) return pmem_read(addr, len);
-// #endif
-
 
   IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pread(addr, len));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
@@ -89,14 +86,12 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-// #ifdef CONFIG_TARGET_SHARE
   if (in_dev(addr)) {dev_skip = true; return;}
 
   if (in_sram(addr)) { pmem_write(addr, len, data); return; }
   if (in_flash(addr)) { pmem_write(addr, len, data); return; }
   if (in_sdram(addr)) { pmem_write(addr, len, data); return; }
   if (in_psram(addr)) { pmem_write(addr, len, data); return; }
-// #endif
 
   IFDEF(CONFIG_MTRACE, if(addr >= CONFIG_MTRACE_START && addr <= CONFIG_MTRACE_END) display_pwrite(addr, len, data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
