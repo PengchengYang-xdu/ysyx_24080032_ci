643b5a63e6e55523de4b8b5ff6494abdb9ec1c15
tracer-ysyx <tracer@ysyx.org>
2025-01-24 13:22:22 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:22:22 up 2 days, 17:58,  1 user,  load average: 1.18, 1.15, 1.10

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -24,7 +24,7 @@ LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall -Werror -g $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
-CFLAGS += -DCONFIG_ICACHESIM
+
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
