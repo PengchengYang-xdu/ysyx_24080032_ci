0f91512428c5beb37a89d41e7e08f91393347510
tracer-ysyx <tracer@ysyx.org>
2025-01-24 13:38:46 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:38:46 up 2 days, 18:15,  1 user,  load average: 1.68, 1.39, 1.24

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,7 +12,7 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x30000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
-NEMUFLAGS += -b
+# NEMUFLAGS += -b
 # '-b' added by ypc 20240919
 NEMUFLAGS += -e $(IMAGE).elf
 # '-e' added by ypc 20240919
