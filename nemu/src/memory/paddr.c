8ad4a625b07fd5cd687b2361b6cc644fa35230ad
tracer-ysyx <tracer@ysyx.org>
2025-01-24 11:01:36 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:01:36 up 2 days, 15:37,  1 user,  load average: 1.10, 1.14, 1.10

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -63,7 +63,6 @@ static void out_of_bound(paddr_t addr) {
 
 void init_mem() {
 #if   defined(CONFIG_PMEM_MALLOC)
-printf("CONFIG_PMEM_MALLOC set\n");
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
