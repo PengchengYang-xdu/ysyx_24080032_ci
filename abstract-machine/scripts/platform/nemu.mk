f0596820686d084d26026645ea0e052747381afc
tracer-ysyx <tracer@ysyx.org>
2025-01-24 13:25:07 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:25:07 up 2 days, 18:01,  1 user,  load average: 1.05, 1.12, 1.09

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -48,4 +48,4 @@ icachesim:
 #首先制作ysyxsoc的microbench train程序流
 	$(MAKE) -C $(MICROBENCH_HOME) ARCH=riscv32e-ysyxsoc mainargs=test
 #之后用nemu执行ysyxsoc的程序流, 从而生成icachesim.log
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(YSYXSOC_IMAGE).bin
+	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(YSYXSOC_IMAGE).bin CFLAGS="$(CFLAGS)"
