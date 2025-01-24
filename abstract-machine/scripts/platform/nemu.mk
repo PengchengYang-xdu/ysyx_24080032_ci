88fe549ec5dbe8a812b92831177f999880d89610
tracer-ysyx <tracer@ysyx.org>
2025-01-24 13:20:01 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:20:01 up 2 days, 17:56,  1 user,  load average: 1.07, 1.12, 1.09

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -37,7 +37,7 @@ gdb: image
 
 
 MICROBENCH_HOME = /home/ypc/Desktop/ysyx/ysyx-workbench/am-kernels/benchmarks/microbench
-CFLAGS += -DCONFIG_ICACHESIM
+
 ifeq ($(MAKECMDGOALS),icachesim)
 CFLAGS += -DCONFIG_ICACHESIM
 endif
@@ -47,7 +47,5 @@ YSYXSOC_IMAGE := $(subst nemu,ysyxsoc,$(IMAGE))
 icachesim:
 #首先制作ysyxsoc的microbench train程序流
 	$(MAKE) -C $(MICROBENCH_HOME) ARCH=riscv32e-ysyxsoc mainargs=test
-	@echo "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n"
-	@echo "CFLAGS: $(CFLAGS)"
 #之后用nemu执行ysyxsoc的程序流, 从而生成icachesim.log
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(YSYXSOC_IMAGE).bin
