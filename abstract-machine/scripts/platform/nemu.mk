dac1c342cf174ba880da5d7bda1aa936f9dfd89b
tracer-ysyx <tracer@ysyx.org>
2025-01-24 13:29:28 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:29:28 up 2 days, 18:05,  1 user,  load average: 1.10, 1.09, 1.08

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -38,9 +38,9 @@ gdb: image
 
 MICROBENCH_HOME = /home/ypc/Desktop/ysyx/ysyx-workbench/am-kernels/benchmarks/microbench
 
-ifeq ($(MAKECMDGOALS),icachesim)
-CFLAGS += -DCONFIG_ICACHESIM
-endif
+# ifeq ($(MAKECMDGOALS),icachesim)
+# CFLAGS += -DCONFIG_ICACHESIM
+# endif
 
 YSYXSOC_IMAGE := $(subst nemu,ysyxsoc,$(IMAGE))
 
