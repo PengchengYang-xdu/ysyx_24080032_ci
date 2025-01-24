bcbce8ffab413a281a3b67349751440a8535421c
tracer-ysyx <tracer@ysyx.org>
2025-01-24 11:25:38 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:25:38 up 2 days, 16:01,  1 user,  load average: 1.02, 1.08, 1.08

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -13,6 +13,12 @@ static const char mainargs[] = MAINARGS;
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
+void print_string(const char *str) {
+    while (*str != '\0') {  // 遍历字符串直到遇到空字符
+        putch(*str);        // 打印当前字符
+        str++;              // 移动到下一个字符
+    }
+}
 
 void halt(int code) {
   nemu_trap(code);
@@ -22,6 +28,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+    print_string(&_heap_start);
   int ret = main(mainargs);
   halt(ret);
 }
