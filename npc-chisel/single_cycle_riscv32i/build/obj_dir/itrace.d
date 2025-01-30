d9ddfd6d86f24a8b33b62d41375f12ede3675435
tracer-ysyx <tracer@ysyx.org>
2025-01-30 23:38:45 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:45 up 5 days,  8:35,  1 user,  load average: 1.20, 1.25, 1.27

---
+++ npc-chisel/single_cycle_riscv32i/build/obj_dir/itrace.d
@@ -0,0 +1,4 @@
+itrace.o: \
+ /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/csrc/utils/itrace.cpp \
+ /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/common.h \
+ /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/utils.h
