5c8efb0cb2214a2e24e6c43fb655fb466fed80c5
tracer-ysyx <tracer@ysyx.org>
2025-01-30 23:15:45 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:15:45 up 5 days,  8:12,  1 user,  load average: 1.52, 1.68, 1.48

--- npc-chisel/single_cycle_riscv32i/build/obj_dir/ftrace.d
+++
@@ -1,4 +0,0 @@
-ftrace.o: \
- /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/csrc/utils/ftrace.cpp \
- /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/common.h \
- /home/ypc/Desktop/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/utils.h
