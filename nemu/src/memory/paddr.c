1731c7c06d0dca9dcfaa8475f842e8e301ee18f4
tracer-ysyx <tracer@ysyx.org>
2025-01-30 23:14:27 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:27 up 5 days,  8:10,  1 user,  load average: 2.01, 1.73, 1.47

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -81,7 +81,7 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
     // printf("read_now, addr = 0x%x\n", addr);
 // #ifdef CONFIG_TARGET_SHARE
-  if (in_dev(addr)) {dev_skip = true; return 0;}
+//   if (in_dev(addr)) {dev_skip = true; return 0;}
 
   if (in_mrom(addr)) return pmem_read(addr, len);
   if (in_sram(addr)) return pmem_read(addr, len);
@@ -113,7 +113,7 @@ word_t paddr_read(paddr_t addr, int len) {
 void paddr_write(paddr_t addr, int len, word_t data) {
     // printf("write_now, addr = 0x%x\n", addr);
 // #ifdef CONFIG_TARGET_SHARE
-  if (in_dev(addr)) {dev_skip = true; return;}
+//   if (in_dev(addr)) {dev_skip = true; return;}
 
   if (in_sram(addr)) { pmem_write(addr, len, data); return; }
   if (in_flash(addr)) { pmem_write(addr, len, data); return; }
