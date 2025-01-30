e9bd5abe229a434cd68ea0949be07f75b05d0756
tracer-ysyx <tracer@ysyx.org>
2025-01-30 22:36:12 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ypc 6.8.0-47-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct  2 16:16:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:12 up 5 days,  7:32,  1 user,  load average: 1.56, 1.85, 1.56

--- nemu/src/ysyxsoc/mem/ysyxsoc_mem.c
+++ nemu/src/ysyxsoc/mem/ysyxsoc_mem.c
@@ -37,29 +37,29 @@ void init_psram(){
 
 
 
-void init_ysyxsoc_mem(){
-#ifdef CONFIG_TARGET_SHARE
-    init_mrom();
-    init_sram();
-    init_flash();
-    init_sdram();
-    init_psram();
-    printf("init soc mem success!\n");
-    init_icachesim();
-    printf("init icachesim success!\n");
-#elif CONFIG_ICACHESIM
-    init_mrom();
-    init_sram();
-    init_flash();
-    init_sdram();
-    init_psram();
-    printf("init soc mem success!\n");
-    init_icachesim();
-    printf("init icachesim success!\n");
-#endif
-    init_icachesim();
-    printf("init icachesim success!\n");
-}
+// void init_ysyxsoc_mem(){
+// #ifdef CONFIG_TARGET_SHARE
+//     init_mrom();
+//     init_sram();
+//     init_flash();
+//     init_sdram();
+//     init_psram();
+//     printf("init soc mem success!\n");
+//     init_icachesim();
+//     printf("init icachesim success!\n");
+// #elif CONFIG_ICACHESIM
+//     init_mrom();
+//     init_sram();
+//     init_flash();
+//     init_sdram();
+//     init_psram();
+//     printf("init soc mem success!\n");
+//     init_icachesim();
+//     printf("init icachesim success!\n");
+// #endif
+//     init_icachesim();
+//     printf("init icachesim success!\n");
+// }
 
 
 bool in_mrom(paddr_t addr){
