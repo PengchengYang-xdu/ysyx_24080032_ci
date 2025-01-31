6e6d6e56f9dca1aeaa2276d7d9571bbb5df83d2f
tracer-ysyx <tracer@ysyx.org>
2025-01-31 12:40:36 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:40:36 up 86 days, 18:20,  4 users,  load average: 10.89, 10.86, 10.37

--- nemu/src/ysyxsoc/mem/ysyxsoc_mem.c
+++ nemu/src/ysyxsoc/mem/ysyxsoc_mem.c
@@ -114,7 +114,7 @@ bool in_dev(paddr_t addr) {
 
 
 static FILE *icache_fp = NULL;
-static char * icache_file = "/mnt/hgfs/share/icachesim/icachesim.log";
+static char * icache_file = "/home/yangpengcheng/ysyx/ysyx/icachesim.log";
 
 void init_icachesim(){
     icache_fp = fopen(icache_file, "w");
