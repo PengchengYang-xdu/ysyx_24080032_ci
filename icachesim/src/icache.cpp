53441f365523577e9919a1048a3144b13aebc6d1
tracer-ysyx <tracer@ysyx.org>
2025-02-13 09:40:58 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:40:58 up 99 days, 15:20,  4 users,  load average: 2.87, 2.76, 3.08

---
+++ icachesim/src/icache.cpp
@@ -0,0 +1,113 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <icache.h>
+
+
+iCache *init_icache(uint sets, uint ways, ReplacementPolicy policy){
+    iCache *icache = (iCache *)malloc(sizeof(iCache));
+    icache->sets = sets;
+    icache->ways = ways;
+    icache->policy = policy;
+    icache->blocks = (iCacheBlock *)malloc(sets * ways * sizeof(iCacheBlock));
+
+    icache->lru_history = (int *)malloc(sets * sizeof(int));
+    icache->fifo_queue = (uint *)malloc(sets * sizeof(uint));
+
+    for (int i = 0; i < sets; i++) {
+        for(int j = 0; j < ways; j++){
+            icache->blocks[i * ways + j].valid = 0;
+            icache->blocks[i * ways + j].tag = 0;
+        }
+    }
+    if (policy == LRU) {
+        for(int i = 0; i < sets; i++)
+            icache->lru_history[i] = -1;
+    } else if (policy == FIFO) {
+        for(int i = 0; i < sets; i++)
+            icache->fifo_queue[i] = 0;
+    }
+
+    return icache;
+}
+
+int lookup_icache(iCache *cache, uint address, ReplacementPolicy policy){
+    uint index = get_index(address, BLOCK_SIZE, cache->sets);
+    uint tag = get_tag(address, BLOCK_SIZE, cache->sets);
+    int hit = 0;
+
+    //查找index对应的icache组
+    for(int j = 0; j < cache->ways; j++){
+        uint idx = index * cache->ways + j;
+        if (cache->blocks[idx].valid && cache->blocks[idx].tag == tag) {
+            hit = 1;
+            if (policy == LRU) {
+                // 更新LRU历史记录
+                cache->lru_history[idx] = time(NULL);
+            }
+            break;
+        }
+    }
+
+    //如果没命中
+    if(!hit) {
+        //替换索引
+        int replace_index = -1;
+
+        //遍历查找这个组中是否存在空的cache块, 如果是, 那么替换就变成了填充
+        for(int j = 0; j < cache->ways; j++){
+            uint idx = index * cache->ways + j;
+            if (cache->blocks[idx].valid == 0){
+                replace_index = idx;
+                break;
+            }
+        }
+
+        if(replace_index == -1){
+            switch (policy) {
+                case FIFO:
+                    replace_index = cache->fifo_queue[index];
+                    break;
+                case LRU:
+                    replace_index = index * cache->ways + cache->lru_history[index];
+                    break;
+                case RANDOM:
+                    replace_index = index;//改一下
+                    break;
+            }
+        }
+        // 替换操作
+        cache->blocks[replace_index].valid = 1;
+        cache->blocks[replace_index].tag = tag;
+
+        if(policy == FIFO){
+            cache->fifo_queue[index] = (cache->fifo_queue[index] + 1) % cache->ways;
+        } else if(policy == LRU){
+            cache->lru_history[index] = time(NULL);
+        }
+    }
+    
+    return hit;
+}
+
+uint get_tag(uint address, uint block_size, uint sets) {
+    uint m = log2(block_size);
+    uint n = log2(sets);
+    return address >> (m + n);
+}
+
+uint get_index(uint address, uint block_size, uint sets) {
+    uint m = log2(block_size);
+    uint n = log2(sets);
+    return (address >> m) & ((1 << n - 1));
+}
+
+void free_cache(iCache *cache) {
+    free(cache->blocks);
+    free(cache->lru_history);
+    free(cache->fifo_queue);
+    free(cache);
+}
