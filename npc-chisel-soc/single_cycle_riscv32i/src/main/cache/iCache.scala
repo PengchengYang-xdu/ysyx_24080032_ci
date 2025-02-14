64762b2ee8250121052b54a263db12030f4b9406
tracer-ysyx <tracer@ysyx.org>
2025-02-14 20:16:23 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:23 up 101 days,  1:56,  5 users,  load average: 5.42, 4.87, 4.78

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -20,10 +20,18 @@ class iCacheBlock(val m: Int, val n: Int) extends Bundle{
 
 class iCacheSet(val m: Int, val n: Int, val ways: Int, val ways_width: Int, val replacementPolicy: String) extends Bundle{
     val set = Vec(ways, new iCacheBlock(m, n))
-    if(replacementPolicy == "LRU"){
-        val lruMatrix = Vec(ways, Vec(ways, UInt(1.W)))
-    } else if(replacementPolicy == "FIFO"){
-        val fifoPtr = RegInit(0.U(ways_width.W))
+    // if(replacementPolicy == "LRU"){
+    //     val lruMatrix = Vec(ways, Vec(ways, UInt(1.W)))
+    // } else if(replacementPolicy == "FIFO"){
+    //     val fifoPtr = RegInit(0.U(ways_width.W))
+    // }
+    val lruMatrix = replacementPolicy match{
+        case "LRU" => Some(Vec(ways, Vec(ways, UInt(1.W))))
+        case _ => None
+    }
+    val fifoPtr = replacementPolicy match{
+        case "FIFO" => Some(RegInit(0.U(ways_width.W)))
+        case _ => None
     }
 }
 
