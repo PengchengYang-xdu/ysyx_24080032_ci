e9deb9ad7991f2552271003f211b56a4e84f7be8
tracer-ysyx <tracer@ysyx.org>
2025-02-14 20:26:20 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:26:20 up 101 days,  2:06,  5 users,  load average: 5.27, 5.52, 5.27

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -21,7 +21,7 @@ class iCacheBlock(val m: Int, val n: Int) extends Bundle{
 class iCacheSet(val m: Int, val n: Int, val ways: Int, val ways_width: Int) extends Bundle{
     val set = Vec(ways, new iCacheBlock(m, n))
     val lruMatrix = Vec(ways, Vec(ways, UInt(1.W)))
-    val fifoPtr = RegInit(0.U(ways_width.W))
+    val fifoPtr = UInt(ways_width.W)
 }
 
 class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementPolicy: String) extends Module{
