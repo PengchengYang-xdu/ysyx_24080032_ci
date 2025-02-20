31b8901ed2f47557ae678a264269a513d83151c7
tracer-ysyx <tracer@ysyx.org>
2025-02-20 18:55:57 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:57 up 107 days, 35 min,  2 users,  load average: 17.32, 16.07, 16.30

--- npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
@@ -183,12 +183,13 @@ class Xbar extends Module {
         s_clint_0    ->  Mux(clint_done0, s_clint_1, s_clint_0),
         s_clint_1    ->  Mux(clint_done1, s_IDLE, s_clint_1)
     ))
-    DefaultImem()
-    DefaultDmem()
-    DefaultSoc()
-    DefaultClint()
+
     switch(n_state){//third phase
         is(s_IDLE){
+            DefaultImem()
+            DefaultDmem()
+            DefaultSoc()
+            DefaultClint()
         }
         is(s_soc_i_0){
             ConnectImem2Soc()
@@ -358,7 +359,7 @@ class Xbar extends Module {
         dmem_rvalid := false.B
         dmem_rlast := true.B
         dmem_rid := 0.U
-        dmem_awready := false.B
+        dmem_awready := true.B
         dmem_wready := true.B
         dmem_bresp := 0.U
         dmem_bvalid := false.B
