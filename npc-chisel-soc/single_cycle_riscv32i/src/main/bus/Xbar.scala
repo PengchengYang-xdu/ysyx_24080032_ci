655c9d72fafc9696ce4b53dc95cc9d3a0dd94d8c
tracer-ysyx <tracer@ysyx.org>
2025-02-21 16:14:52 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:52 up 107 days, 21:54,  1 user,  load average: 18.77, 18.38, 17.65

--- npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
@@ -498,7 +498,7 @@ class Xbar extends Module {
 
     val soc_i_done = ~io.soc.rvalid & soc_rvalid_r & soc_rready & burstCnt === 0.U
     val soc_d_done = (~io.soc.rvalid & soc_rvalid_r & soc_rready) | (~io.soc.bvalid & soc_bvalid_r & soc_bready)
-    val clint_d_done = (~io.clint.rvalid & clint_rvalid_r & io.clint.rready) | (~io.clint.bvalid & clint_bvalid_r & io.clint.bready)
+    val clint_d_done = (~io.clint.rvalid & clint_rvalid_r) | (~io.clint.bvalid & clint_bvalid_r)
 
     c_state := n_state//first phase
 
