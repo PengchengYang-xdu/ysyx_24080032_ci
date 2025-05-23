8a7e75f561e32bdfe97a39787b6e670122515be2
tracer-ysyx <tracer@ysyx.org>
2025-05-23 21:47:23 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:23 up 3 days, 10:03,  4 users,  load average: 1.37, 1.16, 1.22

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -159,18 +159,18 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
         }
         is(s_i_0){
             ConnectIn2Out()
-            out_araddr := addr_align + (c.U << 2)
+            out_araddr := Mux(issdram_raddr, Mux(c.U === 1.U, io.in.araddr, addr_align), io.in.araddr)
             out_arvalid := ~hit0
         }
         is(s_i_1){
             ConnectIn2Out()
-            out_araddr := addr_align + (c.U << 2)
+            out_araddr := Mux(issdram_raddr, Mux(c.U === 1.U, io.in.araddr, addr_align), io.in.araddr)
             out_arvalid := false.B
             out_rready := true.B
         }
         is(s_i_2){
             ConnectIn2Out()
-            out_araddr := addr_align + (c.U << 2)
+            out_araddr := Mux(issdram_raddr, Mux(c.U === 1.U, io.in.araddr, addr_align), io.in.araddr)
             out_rready := false.B
         }
     }
