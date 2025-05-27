b405548580a89f7a2aad33b47db882bef77be47d
tracer-ysyx <tracer@ysyx.org>
2025-05-27 18:33:19 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:33:19 up 7 days,  6:49,  1 user,  load average: 1.43, 2.22, 2.47

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -136,7 +136,7 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
     val hit0 = RegEnable(ways_hit, n_state === s_icache_lookup)
 
 
-    val icache_wdata_index = c.U - count - 1.U
+    val icache_wdata_index = Mux(out_arlen === 0.U, c.U - count - 1.U, c.U - count)
     val icache_wdata = RegInit(VecInit(Seq.fill(c)(0.U(32.W))))
     icache_wdata(icache_wdata_index(log2Ceil(c)-1, 0)) := Mux(out_arlen === 0.U,
         Mux((n_state === s_i_2 || n_state === s_i_0) && c_state === s_i_1, io.out.rdata, icache_wdata(icache_wdata_index(log2Ceil(c)-1, 0))),
@@ -271,7 +271,7 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
 
 
     when(n_state === s_icache_lookup){
-        count := Mux(out_arlen === 0.U, c.U, c.U - 1.U)
+        count := c.U
     }.elsewhen(count =/= 0.U && ((out_arlen === 0.U && c_state === s_i_0 && n_state === s_i_1) || (out_arlen =/= 0.U && out_rready && io.out.rvalid))){
         count := count - 1.U
     }
