269d670140c0848a6cc59de9c5919eda0c1c25b6
tracer-ysyx <tracer@ysyx.org>
2025-05-27 14:55:02 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:02 up 7 days,  3:11,  1 user,  load average: 15.24, 15.36, 11.77

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -136,9 +136,9 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
     val hit0 = RegEnable(ways_hit, n_state === s_icache_lookup)
 
 
-    val icache_wdata_index = (c.U - count - 1.U).asUInt()(log2Ceil(c)-1, 0)
+    val icache_wdata_index = c.U - count - 1.U
     val icache_wdata = RegInit(VecInit(Seq.fill(c)(0.U(32.W))))
-    icache_wdata(icache_wdata_index) := Mux((n_state === s_i_2 || n_state === s_i_0) && c_state === s_i_1, io.out.rdata, icache_wdata(icache_wdata_index))
+    icache_wdata(icache_wdata_index(log2Ceil(c)-1, 0)) := Mux((n_state === s_i_2 || n_state === s_i_0) && c_state === s_i_1, io.out.rdata, icache_wdata(icache_wdata_index))
 
 
 
