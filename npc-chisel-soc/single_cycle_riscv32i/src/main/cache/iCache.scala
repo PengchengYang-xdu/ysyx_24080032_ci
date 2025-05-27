f0c3dcf63bcd2ec543d944618805e1c0edbf8001
tracer-ysyx <tracer@ysyx.org>
2025-05-27 15:58:49 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:49 up 7 days,  4:14,  1 user,  load average: 0.54, 1.70, 2.98

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -208,8 +208,13 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
         }
     }
 
-    in_rdata := Mux(issdram_raddr, Mux(n_state === s_icache_lookup && hit0, icache(req_index).set(ways_hit_num).data(req_offset >> 2), 
-    Mux(n_state === s_i_2, icache(req_index).set(emptyIndex).data(req_offset >> 2), in_rdata)), Mux(n_state === s_i_2, io.out.rdata, in_rdata))
+    in_rdata := Mux(issdram_raddr,
+    Mux(n_state === s_icache_lookup && hit0,
+        icache(req_index).set(ways_hit_num).data(req_offset >> 2),
+        Mux(io.out.rvalid & out_rready && ((c.U - 1.U - count) == req_offset >> 2),
+            io.out.rdata,
+            in_rdata)),
+    Mux(n_state === s_i_2, io.out.rdata, in_rdata))
 
 
     //命中的时候更新LRU矩阵
