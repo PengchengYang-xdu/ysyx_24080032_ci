f063a873a6ed4a1c052cdb75fbdc0c86083ad8a8
tracer-ysyx <tracer@ysyx.org>
2025-05-28 14:46:38 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:46:38 up 8 days,  3:02,  1 user,  load average: 16.42, 15.32, 14.65

--- npc-chisel-soc/single_cycle_riscv32i/nvboard/build/obj_dir/VysyxSoCFull.d
+++ npc-chisel-soc/single_cycle_riscv32i/nvboard/build/obj_dir/VysyxSoCFull.d
@@ -6,9 +6,10 @@ VysyxSoCFull.o: VysyxSoCFull.cpp VysyxSoCFull.h \
  /usr/local/share/verilator/include/verilated_funcs.h \
  /usr/local/share/verilator/include/vltstd/svdpi.h VysyxSoCFull__Syms.h \
  VysyxSoCFull___024root.h VysyxSoCFull___024unit.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
+ /usr/local/share/verilator/include/verilated_fst_c.h \
  /usr/local/share/verilator/include/verilated.h \
  /usr/local/share/verilator/include/verilated_trace.h \
  /usr/local/share/verilator/include/verilated_trace_defs.h \
+ /usr/local/share/verilator/include/gtkwave/fstapi.h \
  /usr/local/share/verilator/include/verilated_dpi.h \
  /usr/local/share/verilator/include/verilated_sym_props.h
--- npc-chisel-soc/single_cycle_riscv32i/nvboard/build/obj_dir/VysyxSoCFull__Dpi.h
+++ npc-chisel-soc/single_cycle_riscv32i/nvboard/build/obj_dir/VysyxSoCFull__Dpi.h
@@ -18,17 +18,17 @@ extern "C" {
     // DPI IMPORTS
     // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/perip/flash/flash.v:84:30
     extern void flash_read(int addr, int* data);
-    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:55763:30
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8168:30
     extern void mrom_read(int raddr, int* rdata);
     // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/vsrc/Ebreak.sv:7:30
     extern void npc_trap();
-    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:55784:30
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8189:30
     extern void psram_read(int addr, int* rdata);
-    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:55785:30
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8190:30
     extern void psram_write(int addr, int wdata, char wstrb);
-    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:55839:30
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8244:30
     extern void sdram_read(int id, int bank_addr, int row_addr, int col_addr, int* rdata);
-    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:55840:30
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8245:30
     extern void sdram_write(int id, int bank_addr, int row_addr, int col_addr, int wdata, char wstrb);
 
 #ifdef __cplusplus
