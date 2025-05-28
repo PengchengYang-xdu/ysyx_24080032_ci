825763488b16dc265abd2d515963eaebde017c6c
tracer-ysyx <tracer@ysyx.org>
2025-05-28 19:00:04 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:00:04 up 8 days,  7:16,  1 user,  load average: 14.24, 14.12, 14.02

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
@@ -244,8 +244,8 @@ class IDU extends Module {
         }
         is(s_AfterPreFire){
             in_ready := false.B
-            out_valid := true.B
-            is_fencei_valid := true.B
+            out_valid := Mux(is_fencei === 1.U, false.B, true.B)
+            is_fencei_valid := Mux(is_fencei === 1.U, true.B, false.B)
         }
     }
 
@@ -254,6 +254,6 @@ class IDU extends Module {
 
 
 
-    is_fencei_reg := Mux(fencei_io_vr.is_fencei_io.fire, 0.U, is_fencei)
+    is_fencei_reg := Mux(fencei_io_vr.is_fencei_io.fire, 0.U, Mux(io_pipe.in.valid, is_fencei, is_fencei_reg))
 }
 
