515ae938807d42b1c95a80e3dbe5131cfa01d361
tracer-ysyx <tracer@ysyx.org>
2025-05-28 19:25:44 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:25:44 up 8 days,  7:41,  1 user,  load average: 20.08, 19.35, 17.32

---
+++ npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/VysyxSoCFull__Dpi.h
@@ -0,0 +1,38 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VYSYXSOCFULL__DPI_H_
+#define VERILATED_VYSYXSOCFULL__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/perip/flash/flash.v:84:30
+    extern void flash_read(int addr, int* data);
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8168:30
+    extern void mrom_read(int raddr, int* rdata);
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/vsrc/Ebreak.sv:7:30
+    extern void npc_trap();
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8189:30
+    extern void psram_read(int addr, int* rdata);
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8190:30
+    extern void psram_write(int addr, int wdata, char wstrb);
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8244:30
+    extern void sdram_read(int id, int bank_addr, int row_addr, int col_addr, int* rdata);
+    // DPI import at /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/ysyxSoC/build/ysyxSoCFull.v:8245:30
+    extern void sdram_write(int id, int bank_addr, int row_addr, int col_addr, int wdata, char wstrb);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
