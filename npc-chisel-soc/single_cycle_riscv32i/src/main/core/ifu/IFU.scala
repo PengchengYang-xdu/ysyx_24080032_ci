5ef773e3a1c89e25486dcea6b05fa2fcf2c13f1c
tracer-ysyx <tracer@ysyx.org>
2025-05-30 10:50:20 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:50:20 up 9 days, 23:06,  1 user,  load average: 14.13, 15.02, 14.73

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -7,6 +7,11 @@ import npc.common.Instructions._
 import npc.core.wbu._
 import npc.bus.axi._
 
+class IFUIO_HAZARD extends Bundle {
+    val flush_flg = Input(Bool())
+}
+
+
 class IFUIO extends Bundle {
     val imem = Flipped(new AXI4WithoutClk)
 
@@ -33,6 +38,10 @@ class IFUIO_pipe extends Bundle {
 class IFU extends Module {
     val io = IO(new IFUIO)
     val io_pipe = IO(new IFUIO_pipe)
+
+
+    val io_hazard = IO(new IFUIO_HAZARD)
+
 
     //disable AW W B and something in AR R
     io.imem.arid := 0.U
