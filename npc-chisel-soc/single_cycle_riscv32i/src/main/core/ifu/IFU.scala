7b192b1e4c855a23fb0c6743e8b092b856e1f3b4
tracer-ysyx <tracer@ysyx.org>
2025-05-30 15:42:07 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:07 up 10 days,  3:58,  1 user,  load average: 19.17, 17.84, 16.43

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -216,6 +216,6 @@ class IFU extends Module {
     //auto fetch logic
     val auto_valid = RegInit(false.B)
     auto_valid := Mux(n_state === s_BeforePreFire && io_hazard.flush_flg, true.B, RegEnable(true.B, false.B, io_pipe.in.ready))
-    ifu.io_pipe.in.valid := auto_valid
+    io_pipe.in.valid := auto_valid
 }
 
