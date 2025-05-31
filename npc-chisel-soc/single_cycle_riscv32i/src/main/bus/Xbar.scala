1384505cc5ddd90f5e07228b01bbf5889d7a5230
tracer-ysyx <tracer@ysyx.org>
2025-05-31 22:17:28 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:17:28 up 11 days, 10:33,  1 user,  load average: 16.92, 16.80, 17.16

--- npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
@@ -582,8 +582,11 @@ class Xbar extends Module {
 
     io.dmem.arready := Mux(c_state === s_IDLE, true.B, false.B)
     io.dmem.awready := Mux(c_state === s_IDLE, true.B, false.B)
+    io.dmem.wready  := Mux(c_state === s_IDLE, true.B, false.B)
+
     io.imem.arready := Mux(c_state === s_IDLE, true.B, false.B)
-    io.imem.awready := Mux(c_state === s_IDLE, true.B, false.B)
+    io.imem.awready := Mux(c_state === s_IDLE, false.B, false.B)
+    io.imem.wready  := Mux(c_state === s_IDLE, false.B, false.B)
 
 
 /*-----------------------function-----------------------*/
@@ -595,7 +598,7 @@ class Xbar extends Module {
         io.imem.rlast := io.soc.rlast
         io.imem.rid := io.soc.rid
         // io.imem.awready := io.soc.awready & ~ing_w_or_r
-        io.imem.wready := io.soc.wready
+        // io.imem.wready := io.soc.wready
         io.imem.bresp := io.soc.bresp
         io.imem.bvalid := io.soc.bvalid
         io.imem.bid := io.soc.bid
@@ -628,7 +631,7 @@ class Xbar extends Module {
         io.dmem.rlast := io.soc.rlast
         io.dmem.rid := io.soc.rid
         // io.dmem.awready := io.soc.awready & ~ing_w_or_r
-        io.dmem.wready := io.soc.wready
+        // io.dmem.wready := io.soc.wready
         io.dmem.bresp := io.soc.bresp
         io.dmem.bvalid := io.soc.bvalid
         io.dmem.bid := io.soc.bid
