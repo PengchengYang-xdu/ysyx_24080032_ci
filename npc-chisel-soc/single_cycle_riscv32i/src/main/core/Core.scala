83608f7011e0fff4ff95d1208fee415b4ac46e37
tracer-ysyx <tracer@ysyx.org>
2025-05-31 16:53:48 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:53:48 up 11 days,  5:09,  1 user,  load average: 19.43, 17.46, 16.51

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -104,7 +104,8 @@ class Core extends Module {
     // //auto fetch logic end
     // when(idu.io_hazard.flush_flg){idu.io_pipe.in.valid := false.B}
     // when(exu.io_hazard.flush_flg){exu.io_pipe.in.valid := false.B}
-
+    
+    wbu.io_pipe.out.ready := true.B
     ifu.io_pipe.in.valid := RegNext(ifu.io_pipe.in.ready)
 
 
