9888fb3005e55e0c0726f4b1ed55cda3d2d092e0
tracer-ysyx <tracer@ysyx.org>
2025-06-01 17:29:28 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:29:28 up 12 days,  5:45,  1 user,  load average: 14.38, 16.27, 16.96

--- npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/bus/Xbar.scala
@@ -582,7 +582,7 @@ class Xbar extends Module {
 
     io.dmem.arready := Mux(c_state === s_IDLE, true.B, Mux(c_state === s_d_soc, io.soc.arready, false.B))
     io.dmem.awready := Mux(c_state === s_IDLE, true.B, Mux(c_state === s_d_soc, io.soc.awready, false.B))
-    io.dmem.wready  := Mux(c_state === s_IDLE, true.B, Mux(c_state === s_d_soc, io.soc.wready, false.B))
+    io.dmem.wready  := Mux(c_state === s_IDLE, false.B, Mux(c_state === s_d_soc, io.soc.wready, false.B))
 
     io.imem.arready := Mux(c_state === s_IDLE, true.B, Mux(c_state === s_i_soc, io.soc.arready, false.B))
     io.imem.awready := Mux(c_state === s_IDLE, false.B, Mux(c_state === s_i_soc, io.soc.awready, false.B))
