1d340cb9f65d6286891952d752e6167f453c5d60
tracer-ysyx <tracer@ysyx.org>
2025-06-02 19:20:41 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:41 up 13 days,  7:36,  1 user,  load average: 17.33, 17.41, 17.53

---
+++ npc-chisel/single_cycle_riscv32i/build/obj_dir/watchpoint.d
@@ -0,0 +1,5 @@
+watchpoint.o: \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/csrc/monitor/sdb/watchpoint.cpp \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/csrc/monitor/sdb/sdb.h \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/common.h \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/utils.h
