a70d6d3f4e92e72c43e0bd66500414b0c1204016
tracer-ysyx <tracer@ysyx.org>
2025-06-03 16:35:49 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:49 up 14 days,  4:51,  1 user,  load average: 19.60, 18.71, 17.97

--- npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/difftest/dut.cpp
+++ npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/difftest/dut.cpp
@@ -132,8 +132,7 @@ void difftest_step() {
     int i;
     //get dut reg into CPU_state struct
     CPU_state dut_r;
-    dut_r.pc = DIFF_PC;
-    ref_pre_pc = dut_r.pc;
+    dut_r.pc = ref_r.pc;
     for(i = 0;i < REAL_REGNUM;i++)
       dut_r.gpr[i] = gpr[i];
     for(i = 0;i < 4;i++)
