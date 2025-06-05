a3ae4f24bb2091dbdb9ca7c85d9a39c2e409c666
tracer-ysyx <tracer@ysyx.org>
2025-06-05 22:20:51 +0800

    > sim RTL ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:20:51 up 16 days, 10:36,  1 user,  load average: 17.57, 18.09, 18.15

---
+++ npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/mtrace.d
@@ -0,0 +1,6 @@
+mtrace.o: \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/utils/mtrace.cpp \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/common.h \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/utils.h \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/mem.h \
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/device.h
