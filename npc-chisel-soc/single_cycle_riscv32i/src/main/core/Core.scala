ef52a5e432962ab856500a7355e6db4561981d69
tracer-ysyx <tracer@ysyx.org>
2025-06-05 19:57:41 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:57:41 up 16 days,  8:13,  1 user,  load average: 19.61, 18.80, 19.05

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -117,7 +117,7 @@ class Core extends Module {
     dontTouch(rs2_raw)
 
     val stall_cnt = RegInit(0.U)
-    stall_cnt := Mux(is_raw, Mux((rs1_raw & ~rs2_raw) || (~rs1_raw & rs2_raw), 1.U, 2.U), Mux(wbu_end_flg && stall_cnt =/= 0.U, stall_cnt - 1.U, stall_cnt))
+    stall_cnt := Mux(is_raw, Mux((rs1_raw & ~rs2_raw) || (~rs1_raw & rs2_raw) || (rs1_raw & rs2_raw & (idu.io.gpr_rs1_addr === idu.io.gpr_rs2_addr)), 1.U, 2.U), Mux(wbu_end_flg && stall_cnt =/= 0.U, stall_cnt - 1.U, stall_cnt))
 
     val stall_flg = RegInit(false.B)
     stall_flg := Mux(is_raw, true.B, Mux(stall_cnt === 0.U, false.B, stall_flg))
