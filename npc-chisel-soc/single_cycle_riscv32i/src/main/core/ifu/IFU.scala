3ffb170ebe7c03715440aba1da4b81f3511663d8
tracer-ysyx <tracer@ysyx.org>
2025-06-06 16:34:30 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:30 up 17 days,  4:50,  1 user,  load average: 17.43, 18.36, 19.04

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -86,7 +86,7 @@ class IFU extends Module {
     val in_ready = RegInit(false.B)
     val out_valid = RegInit(false.B)
     io_pipe.in.ready := in_ready
-    io_pipe.out.valid := out_valid
+    io_pipe.out.valid := out_valid & ~io_hazard.flush_flg
 
     val araddr = Wire(UInt(WORD_LEN.W))
     val arvalid = RegInit(false.B)
@@ -173,7 +173,7 @@ class IFU extends Module {
         is(s_AfterPreFire){
             //between modules
             in_ready := false.B
-            out_valid := true.B & ~io_hazard.flush_flg
+            out_valid := true.B
             //AXI
             arvalid := false.B
             rready := false.B
