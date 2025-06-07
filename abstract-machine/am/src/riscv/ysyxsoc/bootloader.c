0b3953e98bbcd3a60ec33dfb63a22d48b3d41319
tracer-ysyx <tracer@ysyx.org>
2025-06-07 22:13:46 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:46 up 18 days, 10:29,  1 user,  load average: 18.14, 18.54, 18.74

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -40,50 +40,26 @@ void bss_clr(){
 //     _trm_init();
 // }
 
-void fsbl() {
-    char *csrc = &_fsbl_end;
-    char *cdest = &_ssbl_start;
-    size_t len = &_ssbl_end - &_ssbl_start;
+void fsbl(){
+    // 以 4 字节单位搬移
+    uint32_t *src = (uint32_t *)&_fsbl_end;
+    uint32_t *dest = (uint32_t *)&_ssbl_start;
 
-    // 优先按4字节搬
-    uint32_t *src = (uint32_t *)csrc;
-    uint32_t *dest = (uint32_t *)cdest;
-    size_t aligned_len = len & ~0x3;
-
-    for (size_t i = 0; i < aligned_len; i += 4) {
+    while ((char *)dest < &_ssbl_end)
         *dest++ = *src++;
-    }
 
-    // 搬运尾部不足4字节
-    csrc = (char *)src;
-    cdest = (char *)dest;
-    for (size_t i = aligned_len; i < len; i++) {
-        *cdest++ = *csrc++;
-    }
-
-    ssbl(&_ssbl_start);
+    // printf("fsbl done\n");
+    ssbl((volatile char *)src);
 }
 
-void ssbl(volatile char *src) {
-    char *csrc = (char *)src;
-    char *cdest = &_text_start;
-    size_t len = &_data_end - &_text_start;
+void ssbl(volatile char *src){
+    uint32_t *psrc = (uint32_t *)src;
+    uint32_t *dest = (uint32_t *)&_text_start;
 
-    uint32_t *psrc = (uint32_t *)csrc;
-    uint32_t *pdest = (uint32_t *)cdest;
-    size_t aligned_len = len & ~0x3;
-
-    for (size_t i = 0; i < aligned_len; i += 4) {
-        *pdest++ = *psrc++;
-    }
-
-    // 尾部不足4字节处理
-    csrc = (char *)psrc;
-    cdest = (char *)pdest;
-    for (size_t i = aligned_len; i < len; i++) {
-        *cdest++ = *csrc++;
-    }
+    while ((char *)dest < &_data_end)
+        *dest++ = *psrc++;
 
     bss_clr();
+    // printf("bootloader done\n");
     _trm_init();
 }
