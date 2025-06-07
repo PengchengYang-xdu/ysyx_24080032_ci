b914cfa545b58a438d1e754720e07d7533903a09
tracer-ysyx <tracer@ysyx.org>
2025-06-07 15:12:43 +0800

    > compile NEMU ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:43 up 18 days,  3:28,  1 user,  load average: 17.93, 18.35, 18.07

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -160,6 +160,12 @@ static int decode_exec(Decode *s) {
   R(0) = 0; // reset $zero to 0
   // printf("pc = %x\n", s->pc);
 
+
+  if(s->isa.inst.val == 0x0037c583){
+    printf("read addr = %x + %x,,,,,,,read data = %x\n", src1, imm, Mr(src1 + imm, 1));
+  }
+
+
   return 0;
 }
 
