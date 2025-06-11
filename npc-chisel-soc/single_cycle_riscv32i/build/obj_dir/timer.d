5f906361d13bed09de125fab5725df37b1607862
tracer-ysyx <tracer@ysyx.org>
2025-06-11 19:48:35 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:48:35 up 22 days,  8:04,  2 users,  load average: 20.04, 20.15, 21.00

--- npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/timer.d
+++
@@ -1,3 +0,0 @@
-timer.o: \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/utils/timer.c \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/common.h
--- npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/watchpoint.d
+++
@@ -1,5 +0,0 @@
-watchpoint.o: \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/monitor/sdb/watchpoint.cpp \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/monitor/sdb/sdb.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/common.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/utils.h
