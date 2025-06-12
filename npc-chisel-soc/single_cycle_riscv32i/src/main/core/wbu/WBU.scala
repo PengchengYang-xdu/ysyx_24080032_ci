2e4cd13869f725e67edae8eaf7722cd8a2486bb8
tracer-ysyx <tracer@ysyx.org>
2025-06-12 15:18:33 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:18:33 up 23 days,  3:34,  2 users,  load average: 19.66, 19.93, 20.27

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/wbu/WBU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/wbu/WBU.scala
@@ -14,6 +14,11 @@ class WBUIO extends Bundle {
     val csr_addr = Output(UInt(CSR_ADDR_LEN.W))
     val csr_cmd = Output(UInt(CSR_LEN.W))
     val csr_wdata = Output(UInt(WORD_LEN.W))
+
+    val is_irq = Output(Bool())
+    val irq_num = Output(UInt(IRQ_NUM_WIDTH.W))
+
+    val irq_valid = Output(Bool())
 }
 
 class WBUIO_pipe_out extends Bundle{
@@ -28,7 +33,7 @@ class WBU extends Module {
     val io = IO(new WBUIO)
     val io_pipe = IO(new WBUIO_pipe)
 
-
+    dontTouch(io_pipe)
 
 
 
@@ -91,5 +96,14 @@ class WBU extends Module {
             out_valid := true.B
         }
     }
+
+
+
+
+
+    //irq
+    io.is_irq := io_pipe.in.bits.ls2wb_is_irq
+    io.irq_num := io_pipe.in.bits.ls2wb_irq_num
+    io.irq_valid := io_pipe.in.valid//接下来补齐lsu wbu的flush flg
 }
 
