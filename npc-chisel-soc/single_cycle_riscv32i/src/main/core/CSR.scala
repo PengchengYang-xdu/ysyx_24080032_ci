89e1fb87aaff841303d9ae5245f5d047fe679637
tracer-ysyx <tracer@ysyx.org>
2025-06-12 20:13:21 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:21 up 23 days,  8:29,  2 users,  load average: 22.00, 21.77, 21.64

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/CSR.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/CSR.scala
@@ -42,11 +42,11 @@ class CSR extends Module {
 
     io.csr_rdata := csr(csr_addr_process)
 
-    when(io.csr_cmd > 0.U && io.csr_is_irq){//modified by ypc
+    when(io.csr_is_irq){//modified by ypc
         csr(CSR_MEPC_ADDR) := io.csr_reg_pc
         csr(CSR_MCAUSE_ADDR) := io.csr_irq_num
     }
-    when(~io.csr_is_irq){
+    when(io.csr_cmd > 0.U && ~io.csr_is_irq){
         csr(csr_addr_process) := io.csr_wdata
     }
 
