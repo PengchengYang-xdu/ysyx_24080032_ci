a96fdc4b103c0681dfaa502c5102dc956b761a5a
tracer-ysyx <tracer@ysyx.org>
2025-06-12 17:23:30 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:30 up 23 days,  5:39,  2 users,  load average: 25.73, 22.75, 21.36

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -102,8 +102,7 @@ class Core extends Module {
 
 
 
-    //irq process
-    val is_irq = wbu.io.irq_valid && wbu.io.is_irq
+
 
 
 
@@ -252,9 +251,15 @@ class Core extends Module {
 
 
     //control hazard
+    val idu_out_valid_rise = idu.io_pipe.out.valid & ~RegNext(idu.io_pipe.out.valid)
+    val is_irq = idu.io_pipe.out.bits.id2exe_is_irq && idu_out_valid_rise
+    dontTouch(is_irq)
+
     val exu_out_valid_rise = exu.io_pipe.out.valid & ~RegNext(exu.io_pipe.out.valid)
     val is_ctrl_hazard = ((exu.io.br_flg && exu.io.br_target =/= ifu.io_pipe.out.bits.if2id_reg_pc) || (exu.io.jmp_flg && exu.io.alu_out =/= ifu.io_pipe.out.bits.if2id_reg_pc)) && exu_out_valid_rise
     dontTouch(is_ctrl_hazard)
+
+
     ifu.io_hazard.flush_flg := is_ctrl_hazard
     idu.io_hazard.flush_flg := is_ctrl_hazard
     exu.io_hazard.flush_flg := is_ctrl_hazard
