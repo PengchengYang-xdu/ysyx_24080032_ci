dbe65bb1a22ea7bb0f08d5e09df12659f82da4d1
tracer-ysyx <tracer@ysyx.org>
2025-06-12 21:01:59 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:59 up 23 days,  9:18,  2 users,  load average: 19.54, 19.87, 20.25

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -82,6 +82,7 @@ class Core extends Module {
     io.dmem <> lsu.io.dmem
     lsu.io.csr_rdata := csr.io.csr_rdata
 
+    csr.io.csr_raddr := idu.io.csr_raddr
     csr.io.csr_wdata := wbu.io.csr_wdata
     csr.io.csr_addr := wbu.io.csr_addr
     csr.io.csr_cmd := wbu.io.csr_cmd
