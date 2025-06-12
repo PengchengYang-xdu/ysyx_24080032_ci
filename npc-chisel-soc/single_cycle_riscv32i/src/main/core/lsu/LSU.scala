4a3d7be042f18be0fdfeedcd766b0c07c8944132
tracer-ysyx <tracer@ysyx.org>
2025-06-12 21:27:46 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:46 up 23 days,  9:43,  2 users,  load average: 20.40, 20.51, 20.73

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/lsu/LSU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/lsu/LSU.scala
@@ -30,9 +30,6 @@ class LSUIO_pipe_out extends Bundle{
     //irq
     val ls2wb_is_irq = Output(Bool())
     val ls2wb_irq_num = Output(UInt(IRQ_NUM_WIDTH.W))
-
-    //csr
-    val ls2wb_csr_rdata = Output(UInt(WORD_LEN.W))
 }
 
 class LSUIO_pipe extends Bundle {
@@ -279,15 +276,15 @@ class LSU extends Module {
     val dmem_rdata_processed = WireDefault(0.U(WORD_LEN.W))
     val csr_wdata =  MuxCase(0.U(WORD_LEN.W), Seq(
         (io_pipe.in.bits.exe2ls_csr_cmd === CSR_W) -> io_pipe.in.bits.exe2ls_op1_data,
-        (io_pipe.in.bits.exe2ls_csr_cmd === CSR_S) -> (io_pipe.in.bits.ls2wb_csr_rdata | io_pipe.in.bits.exe2ls_op1_data),
-        (io_pipe.in.bits.exe2ls_csr_cmd === CSR_C) -> (io_pipe.in.bits.ls2wb_csr_rdata & ~io_pipe.in.bits.exe2ls_op1_data),
+        (io_pipe.in.bits.exe2ls_csr_cmd === CSR_S) -> (io_pipe.in.bits.exe2ls_csr_rdata | io_pipe.in.bits.exe2ls_op1_data),
+        (io_pipe.in.bits.exe2ls_csr_cmd === CSR_C) -> (io_pipe.in.bits.exe2ls_csr_rdata & ~io_pipe.in.bits.exe2ls_op1_data),
         (io_pipe.in.bits.exe2ls_csr_cmd === CSR_E) -> 11.U(WORD_LEN.W)
     ))
 
     val wb_data = MuxCase(io_pipe.in.bits.exe2ls_alu_out, Seq(
         (io_pipe.in.bits.exe2ls_wb_sel === WB_MEM) -> dmem_rdata_processed,
         (io_pipe.in.bits.exe2ls_wb_sel === WB_PC)  -> (io_pipe.in.bits.exe2ls_reg_pc + 4.U(WORD_LEN.W)),
-        (io_pipe.in.bits.exe2ls_wb_sel === WB_CSR) -> io_pipe.in.bits.ls2wb_csr_rdata
+        (io_pipe.in.bits.exe2ls_wb_sel === WB_CSR) -> io_pipe.in.bits.exe2ls_csr_rdata
     ))
 
     switch(io_pipe.in.bits.exe2ls_mem_op) {
