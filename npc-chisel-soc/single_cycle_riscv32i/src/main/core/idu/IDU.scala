412dd3fad2bc713eca205dd1880c929ea2609c14
tracer-ysyx <tracer@ysyx.org>
2025-06-12 21:45:51 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:51 up 23 days, 10:01,  2 users,  load average: 21.63, 21.45, 21.17

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
@@ -22,7 +22,7 @@ class IDUIO extends Bundle {
     val gpr_rs2_is_read = Output(Bool())
 
     val csr_raddr = Output(UInt(CSR_ADDR_LEN.W))
-    val csr_rdata = Input(UInt(IRQ_NUM_WIDTH.W))
+    val csr_rdata = Input(UInt(WORD_LEN.W))
 
     val is_mret = Output(Bool())
 }
