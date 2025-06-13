f7c21cbfe46c2c1f74e45e7a01a3ea0f7d16aae3
tracer-ysyx <tracer@ysyx.org>
2025-06-13 15:15:40 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:40 up 24 days,  3:31,  2 users,  load average: 19.27, 20.40, 20.91

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -217,7 +217,7 @@ class IFU extends Module {
         (io.br_flg && flag)          -> io.br_target,
         (io.jmp_flg && flag)         -> io.alu_out,
         (flag)                       -> io.csr_mtvec,
-        (is_mret)                    -> io.csr_mepc,
+        (io。is_mret)                -> io.csr_mepc,
     ))
     
     //connect
