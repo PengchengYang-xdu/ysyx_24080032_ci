cf91db878ebbd58412238fb148a9745c14965541
tracer-ysyx <tracer@ysyx.org>
2025-06-14 16:11:15 +0800

    > sim RTL ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:15 up 25 days,  4:27,  2 users,  load average: 20.31, 20.73, 21.02

--- npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/lightsss.d
+++ npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/lightsss.d
@@ -1,4 +1,3 @@
 lightsss.o: \
  /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/utils/lightsss.cpp \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/lightsss.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/common.h
+ /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/lightsss.h
