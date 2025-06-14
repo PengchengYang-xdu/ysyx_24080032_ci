1f7703c67ba68fafa07d00a35281f459ee964fd6
tracer-ysyx <tracer@ysyx.org>
2025-06-14 18:10:48 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:10:48 up 25 days,  6:26,  2 users,  load average: 21.29, 20.11, 20.24

--- npc-chisel-soc/single_cycle_riscv32i/csrc/utils/lightsss.cpp
+++ npc-chisel-soc/single_cycle_riscv32i/csrc/utils/lightsss.cpp
@@ -105,7 +105,7 @@ int LightSSS::do_fork() {
     unlink(old_filename);
     snprintf(old_filename, sizeof(old_filename), "/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/build/ysyxsoc_child_%d.fst.hier", temp);
     unlink(old_filename);
-    printf("has remove pid %d\n", temp)
+    printf("has remove pid %d\n", temp);
   }
   // fork a new checkpoint process and block it
   if ((pid = fork()) < 0) {
