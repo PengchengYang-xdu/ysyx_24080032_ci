9edccea7cbc035e01fc2f59bcdaab2db4d29555a
tracer-ysyx <tracer@ysyx.org>
2025-06-14 16:35:55 +0800

    > sim RTL ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:55 up 25 days,  4:51,  2 users,  load average: 21.35, 21.47, 21.06

--- npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/circuit.cpp
+++ npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/circuit.cpp
@@ -332,7 +332,6 @@ static void save2csv(const char *filename){
 
 
 #define FORK_INTERVAL 10000 // 示例：每 10,000 个周期 fork 一次
-#define SLOT_SIZE     10    // 示例：保留 10 个子进程（检查点）
 
 LightSSS lightsss;
 uint64_t light_cycle_num = 0;
