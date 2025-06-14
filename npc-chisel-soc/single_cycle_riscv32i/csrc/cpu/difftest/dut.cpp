5362981243d57fb94083e6ea35142db8927021ad
tracer-ysyx <tracer@ysyx.org>
2025-06-14 20:07:18 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:18 up 25 days,  8:23,  2 users,  load average: 20.33, 20.24, 20.53

--- npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/difftest/dut.cpp
+++ npc-chisel-soc/single_cycle_riscv32i/csrc/cpu/difftest/dut.cpp
@@ -154,8 +154,20 @@ void difftest_step() {
 	itrace_init(PC, INSTR);
 	display_inst();
 	#endif
-    dump_wave();
-	close_wave(2);
+    #if defined(NPCCONFIG_DUMPWAVE) || defined(NPCCONFIG_LIGHTSSS)
+    #ifdef NPCCONFIG_LIGHTSSS
+        if(lightsss.is_child()){
+            dump_wave();
+	        close_wave(2);
+            exit(-1);
+        }else{
+            lightsss.wakeup_child(light_cycle_num);
+        }
+    #else
+        dump_wave();
+	    close_wave(2);
+    #endif
+	#endif
     exit(-1);
   }
 }
