2549d03269b3b8a879dd85b7e0a786e8a0aa58f2
tracer-ysyx <tracer@ysyx.org>
2025-06-14 18:08:22 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:08:22 up 25 days,  6:24,  2 users,  load average: 19.39, 19.47, 20.09

--- npc-chisel-soc/single_cycle_riscv32i/csrc/utils/lightsss.cpp
+++ npc-chisel-soc/single_cycle_riscv32i/csrc/utils/lightsss.cpp
@@ -96,18 +96,15 @@ int LightSSS::do_fork() {
   if (slotCnt == SLOT_SIZE) {
     pid_t temp = pidSlot.back();
     pidSlot.pop_back();
-
-    char old_filename[256];
-    snprintf(old_filename, sizeof(old_filename), "/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/build/ysyxsoc_child_%d.fst", temp);
-    unlink(old_filename);
-    // snprintf(old_filename, sizeof(old_filename), "/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/build/ysyxsoc_child_%d.fst.hier", temp);
-    // unlink(old_filename);
-
-
     kill(temp, SIGKILL);
     int status = 0;
     waitpid(temp, NULL, 0);
     slotCnt--;
+    char old_filename[256];
+    snprintf(old_filename, sizeof(old_filename), "/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/build/ysyxsoc_child_%d.fst", temp);
+    unlink(old_filename);
+    snprintf(old_filename, sizeof(old_filename), "/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/build/ysyxsoc_child_%d.fst.hier", temp);
+    unlink(old_filename);
   }
   // fork a new checkpoint process and block it
   if ((pid = fork()) < 0) {
