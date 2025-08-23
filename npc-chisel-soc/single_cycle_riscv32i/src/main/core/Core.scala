bd0cf36664c01db84128aeddb4516c5302f083a5
tracer-ysyx <tracer@ysyx.org>
2025-08-23 18:18:46 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:18:46 up 95 days,  6:34,  0 users,  load average: 2.19, 2.15, 2.41

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -56,7 +56,7 @@ class Core extends Module {
 
 
 
-    val icache = Module(new iCache(8, 4, 1, "FIFO"))
+    val icache = Module(new iCache(16, 4, 8, "FIFO"))
     io.imem <> icache.io.out
     icache.io.in <> ifu.io.imem
 
