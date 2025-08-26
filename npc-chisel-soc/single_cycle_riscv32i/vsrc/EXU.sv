66074c676308f6ff6980fd8fe73949467c1b7ee9
tracer-ysyx <tracer@ysyx.org>
2025-08-26 21:09:36 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:36 up 22:02,  3 users,  load average: 2.26, 2.33, 1.92

--- npc-chisel-soc/single_cycle_riscv32i/vsrc/EXU.sv
+++ npc-chisel-soc/single_cycle_riscv32i/vsrc/EXU.sv
@@ -93,58 +93,59 @@ module EXU(	// @[src/main/core/exu/EXU.scala:49:7]
   output [31:0] io_pipe_out_bits_exe2ls_csr_rdata	// @[src/main/core/exu/EXU.scala:51:21]
 );
 
-  wire [31:0] _alu_out_T_31 =
-    io_pipe_in_bits_id2exe_op1_data + io_pipe_in_bits_id2exe_op2_data;	// @[src/main/core/exu/EXU.scala:73:92]
-  wire [62:0] _alu_out_T_14 =
-    {31'h0, io_pipe_in_bits_id2exe_op1_data} << io_pipe_in_bits_id2exe_op2_data[4:0];	// @[src/main/core/exu/EXU.scala:78:{92,126}]
-  wire [31:0] _GEN = {27'h0, io_pipe_in_bits_id2exe_op2_data[4:0]};	// @[src/main/core/exu/EXU.scala:78:126, :79:92]
-  wire        _br_flg_T_17 =
-    io_pipe_in_bits_id2exe_op1_data < io_pipe_in_bits_id2exe_op2_data;	// @[src/main/core/exu/EXU.scala:82:92]
+  wire        _alu_out_T_1 = io_pipe_in_bits_id2exe_exe_fun == 5'h2;	// @[src/main/core/exu/EXU.scala:68:86]
+  wire [62:0] _alu_out_T_10 =
+    {31'h0, io_pipe_in_bits_id2exe_op1_data} << io_pipe_in_bits_id2exe_op2_data[4:0];	// @[src/main/core/exu/EXU.scala:80:{92,126}]
+  wire [31:0] _GEN = {27'h0, io_pipe_in_bits_id2exe_op2_data[4:0]};	// @[src/main/core/exu/EXU.scala:80:126, :81:92]
+  wire        ltU = io_pipe_in_bits_id2exe_op1_data < io_pipe_in_bits_id2exe_op2_data;	// @[src/main/core/exu/EXU.scala:84:92]
   wire [31:0] alu_out =
-    io_pipe_in_bits_id2exe_exe_fun == 5'h1
-      ? _alu_out_T_31
-      : io_pipe_in_bits_id2exe_exe_fun == 5'h2
-          ? io_pipe_in_bits_id2exe_op1_data - io_pipe_in_bits_id2exe_op2_data
-          : io_pipe_in_bits_id2exe_exe_fun == 5'h3
-              ? io_pipe_in_bits_id2exe_op1_data & io_pipe_in_bits_id2exe_op2_data
-              : io_pipe_in_bits_id2exe_exe_fun == 5'h4
-                  ? io_pipe_in_bits_id2exe_op1_data | io_pipe_in_bits_id2exe_op2_data
-                  : io_pipe_in_bits_id2exe_exe_fun == 5'h5
-                      ? io_pipe_in_bits_id2exe_op1_data ^ io_pipe_in_bits_id2exe_op2_data
-                      : io_pipe_in_bits_id2exe_exe_fun == 5'h6
-                          ? _alu_out_T_14[31:0]
-                          : io_pipe_in_bits_id2exe_exe_fun == 5'h7
-                              ? io_pipe_in_bits_id2exe_op1_data >> _GEN
-                              : io_pipe_in_bits_id2exe_exe_fun == 5'h8
-                                  ? $signed($signed(io_pipe_in_bits_id2exe_op1_data)
-                                            >>> _GEN)
-                                  : io_pipe_in_bits_id2exe_exe_fun == 5'h9
-                                      ? {31'h0,
-                                         $signed(io_pipe_in_bits_id2exe_op1_data) < $signed(io_pipe_in_bits_id2exe_op2_data)}
-                                      : io_pipe_in_bits_id2exe_exe_fun == 5'hA
-                                          ? {31'h0, _br_flg_T_17}
-                                          : io_pipe_in_bits_id2exe_exe_fun == 5'h11
-                                              ? _alu_out_T_31 & 32'hFFFFFFFE
-                                              : io_pipe_in_bits_id2exe_exe_fun == 5'h12
-                                                  ? io_pipe_in_bits_id2exe_op1_data
-                                                  : 32'h0;	// @[src/main/core/exu/EXU.scala:73:{41,92}, :74:{41,92}, :75:{41,92}, :76:{41,92}, :77:{41,92}, :78:{41,92,133}, :79:{41,92}, :80:{41,99}, :81:{41,99}, :82:{41,92}, :83:{41,128,130}, :84:41, src/main/scala/chisel3/util/Mux.scala:126:16]
-  wire        _br_flg_T_3 =
-    io_pipe_in_bits_id2exe_op1_data == io_pipe_in_bits_id2exe_op2_data;	// @[src/main/core/exu/EXU.scala:88:93]
-  reg         in_ready;	// @[src/main/core/exu/EXU.scala:140:27]
-  reg         out_valid;	// @[src/main/core/exu/EXU.scala:141:28]
-  reg         c_state;	// @[src/main/core/exu/EXU.scala:146:26]
+    io_pipe_in_bits_id2exe_exe_fun == 5'h1 | _alu_out_T_1
+      ? io_pipe_in_bits_id2exe_op1_data
+        + (_alu_out_T_1
+             ? ~io_pipe_in_bits_id2exe_op2_data + 32'h1
+             : io_pipe_in_bits_id2exe_op2_data)
+      : io_pipe_in_bits_id2exe_exe_fun == 5'h3
+          ? io_pipe_in_bits_id2exe_op1_data & io_pipe_in_bits_id2exe_op2_data
+          : io_pipe_in_bits_id2exe_exe_fun == 5'h4
+              ? io_pipe_in_bits_id2exe_op1_data | io_pipe_in_bits_id2exe_op2_data
+              : io_pipe_in_bits_id2exe_exe_fun == 5'h5
+                  ? io_pipe_in_bits_id2exe_op1_data ^ io_pipe_in_bits_id2exe_op2_data
+                  : io_pipe_in_bits_id2exe_exe_fun == 5'h6
+                      ? _alu_out_T_10[31:0]
+                      : io_pipe_in_bits_id2exe_exe_fun == 5'h7
+                          ? io_pipe_in_bits_id2exe_op1_data >> _GEN
+                          : io_pipe_in_bits_id2exe_exe_fun == 5'h8
+                              ? $signed($signed(io_pipe_in_bits_id2exe_op1_data) >>> _GEN)
+                              : io_pipe_in_bits_id2exe_exe_fun == 5'h9
+                                  ? {31'h0,
+                                     $signed(io_pipe_in_bits_id2exe_op1_data) < $signed(io_pipe_in_bits_id2exe_op2_data)}
+                                  : io_pipe_in_bits_id2exe_exe_fun == 5'hA
+                                      ? {31'h0, ltU}
+                                      : io_pipe_in_bits_id2exe_exe_fun == 5'h11
+                                          ? io_pipe_in_bits_id2exe_op1_data
+                                            + io_pipe_in_bits_id2exe_op2_data
+                                            & 32'hFFFFFFFE
+                                          : io_pipe_in_bits_id2exe_exe_fun == 5'h12
+                                              ? io_pipe_in_bits_id2exe_op1_data
+                                              : 32'h0;	// @[src/main/core/exu/EXU.scala:68:{49,54,86}, :69:{55,88}, :75:41, :77:{41,92}, :78:{41,92}, :79:{41,92}, :80:{41,92,133}, :81:{41,92}, :82:{41,99}, :83:{41,99}, :84:{41,92}, :85:{41,93,128,130}, :86:41, src/main/scala/chisel3/util/Mux.scala:126:16]
+  wire        eq = io_pipe_in_bits_id2exe_op1_data == io_pipe_in_bits_id2exe_op2_data;	// @[src/main/core/exu/EXU.scala:91:47]
+  wire        ltS =
+    $signed(io_pipe_in_bits_id2exe_op1_data) < $signed(io_pipe_in_bits_id2exe_op2_data);	// @[src/main/core/exu/EXU.scala:93:55]
+  reg         in_ready;	// @[src/main/core/exu/EXU.scala:147:27]
+  reg         out_valid;	// @[src/main/core/exu/EXU.scala:148:28]
+  reg         c_state;	// @[src/main/core/exu/EXU.scala:153:26]
   wire        n_state =
-    c_state ? ~(io_pipe_out_ready & out_valid) : in_ready & io_pipe_in_valid;	// @[src/main/core/exu/EXU.scala:89:59, :140:27, :141:28, :146:26, :147:30, :152:51, :153:33, :154:33, src/main/scala/chisel3/util/Decoupled.scala:51:35]
+    c_state ? ~(io_pipe_out_ready & out_valid) : in_ready & io_pipe_in_valid;	// @[src/main/core/exu/EXU.scala:69:88, :96:59, :147:27, :148:28, :153:26, :154:30, :159:51, :161:33, src/main/scala/chisel3/util/Decoupled.scala:51:35]
   always @(posedge clock) begin	// @[src/main/core/exu/EXU.scala:49:7]
     if (reset) begin	// @[src/main/core/exu/EXU.scala:49:7]
-      in_ready <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :140:27]
-      out_valid <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :141:28]
-      c_state <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :146:26]
+      in_ready <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :147:27]
+      out_valid <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :148:28]
+      c_state <= 1'h0;	// @[src/main/core/exu/EXU.scala:49:7, :153:26]
     end
     else begin	// @[src/main/core/exu/EXU.scala:49:7]
-      in_ready <= ~n_state | ~n_state & in_ready;	// @[src/main/core/exu/EXU.scala:140:27, :147:30, :157:20, :159:22, :163:22]
-      out_valid <= n_state & (n_state | out_valid);	// @[src/main/core/exu/EXU.scala:141:28, :147:30, :157:20, :160:23, :164:23]
-      c_state <= n_state;	// @[src/main/core/exu/EXU.scala:146:26, :147:30]
+      in_ready <= ~n_state | ~n_state & in_ready;	// @[src/main/core/exu/EXU.scala:147:27, :154:30, :164:20, :166:22, :170:22]
+      out_valid <= n_state & (n_state | out_valid);	// @[src/main/core/exu/EXU.scala:148:28, :154:30, :164:20, :167:23, :171:23]
+      c_state <= n_state;	// @[src/main/core/exu/EXU.scala:153:26, :154:30]
     end
   end // always @(posedge)
   `ifdef ENABLE_INITIAL_REG_	// @[src/main/core/exu/EXU.scala:49:7]
@@ -158,9 +159,9 @@ module EXU(	// @[src/main/core/exu/EXU.scala:49:7]
       `endif // INIT_RANDOM_PROLOG_
       `ifdef RANDOMIZE_REG_INIT	// @[src/main/core/exu/EXU.scala:49:7]
         _RANDOM[/*Zero width*/ 1'b0] = `RANDOM;	// @[src/main/core/exu/EXU.scala:49:7]
-        in_ready = _RANDOM[/*Zero width*/ 1'b0][0];	// @[src/main/core/exu/EXU.scala:49:7, :140:27]
-        out_valid = _RANDOM[/*Zero width*/ 1'b0][1];	// @[src/main/core/exu/EXU.scala:49:7, :140:27, :141:28]
-        c_state = _RANDOM[/*Zero width*/ 1'b0][2];	// @[src/main/core/exu/EXU.scala:49:7, :140:27, :146:26]
+        in_ready = _RANDOM[/*Zero width*/ 1'b0][0];	// @[src/main/core/exu/EXU.scala:49:7, :147:27]
+        out_valid = _RANDOM[/*Zero width*/ 1'b0][1];	// @[src/main/core/exu/EXU.scala:49:7, :147:27, :148:28]
+        c_state = _RANDOM[/*Zero width*/ 1'b0][2];	// @[src/main/core/exu/EXU.scala:49:7, :147:27, :153:26]
       `endif // RANDOMIZE_REG_INIT
     end // initial
     `ifdef FIRRTL_AFTER_INITIAL	// @[src/main/core/exu/EXU.scala:49:7]
@@ -169,21 +170,21 @@ module EXU(	// @[src/main/core/exu/EXU.scala:49:7]
   `endif // ENABLE_INITIAL_REG_
   assign io_br_flg =
     io_pipe_in_bits_id2exe_exe_fun == 5'hB
-      ? _br_flg_T_3
+      ? eq
       : io_pipe_in_bits_id2exe_exe_fun == 5'hC
-          ? ~_br_flg_T_3
+          ? ~eq
           : io_pipe_in_bits_id2exe_exe_fun == 5'hD
-              ? $signed(io_pipe_in_bits_id2exe_op1_data) < $signed(io_pipe_in_bits_id2exe_op2_data)
+              ? ltS
               : io_pipe_in_bits_id2exe_exe_fun == 5'hE
-                  ? $signed(io_pipe_in_bits_id2exe_op1_data) >= $signed(io_pipe_in_bits_id2exe_op2_data)
+                  ? ~ltS
                   : io_pipe_in_bits_id2exe_exe_fun == 5'hF
-                      ? _br_flg_T_17
-                      : io_pipe_in_bits_id2exe_exe_fun == 5'h10 & ~_br_flg_T_17;	// @[src/main/core/exu/EXU.scala:49:7, :82:92, :88:{41,93}, :89:{41,59}, :90:{41,100}, :91:{41,100}, :92:41, :93:{41,59}, src/main/scala/chisel3/util/Mux.scala:126:16]
-  assign io_jmp_flg = io_pipe_in_bits_id2exe_wb_sel == 3'h2;	// @[src/main/core/exu/EXU.scala:49:7, :97:50]
-  assign io_br_target = io_pipe_in_bits_id2exe_reg_pc + io_pipe_in_bits_id2exe_imm_b_sext;	// @[src/main/core/exu/EXU.scala:49:7, :96:51]
+                      ? ltU
+                      : io_pipe_in_bits_id2exe_exe_fun == 5'h10 & ~ltU;	// @[src/main/core/exu/EXU.scala:49:7, :84:92, :91:47, :93:55, :95:41, :96:{41,59}, :97:41, :98:{41,59}, :99:41, :100:{41,59}, src/main/scala/chisel3/util/Mux.scala:126:16]
+  assign io_jmp_flg = io_pipe_in_bits_id2exe_wb_sel == 3'h2;	// @[src/main/core/exu/EXU.scala:49:7, :104:50]
+  assign io_br_target = io_pipe_in_bits_id2exe_reg_pc + io_pipe_in_bits_id2exe_imm_b_sext;	// @[src/main/core/exu/EXU.scala:49:7, :103:51]
   assign io_alu_out = alu_out;	// @[src/main/core/exu/EXU.scala:49:7, src/main/scala/chisel3/util/Mux.scala:126:16]
-  assign io_pipe_in_ready = in_ready;	// @[src/main/core/exu/EXU.scala:49:7, :140:27]
-  assign io_pipe_out_valid = out_valid;	// @[src/main/core/exu/EXU.scala:49:7, :141:28]
+  assign io_pipe_in_ready = in_ready;	// @[src/main/core/exu/EXU.scala:49:7, :147:27]
+  assign io_pipe_out_valid = out_valid;	// @[src/main/core/exu/EXU.scala:49:7, :148:28]
   assign io_pipe_out_bits_exe2ls_reg_pc = io_pipe_in_bits_id2exe_reg_pc;	// @[src/main/core/exu/EXU.scala:49:7]
   assign io_pipe_out_bits_exe2ls_op1_data = io_pipe_in_bits_id2exe_op1_data;	// @[src/main/core/exu/EXU.scala:49:7]
   assign io_pipe_out_bits_exe2ls_rs2_data = io_pipe_in_bits_id2exe_rs2_data;	// @[src/main/core/exu/EXU.scala:49:7]
