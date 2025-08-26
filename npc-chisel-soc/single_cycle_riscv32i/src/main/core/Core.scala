5804a70b014d4351fb6e4e34c6108830b119f523
tracer-ysyx <tracer@ysyx.org>
2025-08-26 22:48:53 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:48:53 up 23:42,  3 users,  load average: 1.56, 1.53, 1.78

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -260,8 +260,8 @@ class Core extends Module {
 
     val is_ctrl_hazard_r = RegInit(false.B)
     val is_irq_r = RegInit(false.B)
-    is_ctrl_hazard_r := Mux(is_ctrl_hazard, true.B, Mux(ifu.io_pipe.in.ready & io_pipe.in.valid, false.B, is_ctrl_hazard_r))
-    is_irq_r := Mux(is_irq, true.B, Mux(ifu.io_pipe.in.ready & io_pipe.in.valid, false.B, is_irq_r))
+    is_ctrl_hazard_r := Mux(is_ctrl_hazard, true.B, Mux(ifu.io_pipe.in.ready & ifu.io_pipe.in.valid, false.B, is_ctrl_hazard_r))
+    is_irq_r := Mux(is_irq, true.B, Mux(ifu.io_pipe.in.ready & ifu.io_pipe.in.valid, false.B, is_irq_r))
 
     ifu.io_hazard.flush_flg := is_ctrl_hazard | is_irq
     idu.io_hazard.flush_flg := is_ctrl_hazard | is_irq
