23fe4a422ff73abf8647c8666a3dd7cfc66abb5f
tracer-ysyx <tracer@ysyx.org>
2025-08-27 16:13:28 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:13:28 up 1 day, 17:06,  3 users,  load average: 0.63, 1.00, 1.25

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -56,7 +56,7 @@ class IFU extends Module {
     io.imem.bready := false.B
 
 
-    val is_mret_rise = io_hazard.is_mret & ~RegNext(io_hazard.is_mret)
+    val is_mret_rise = io_hazard.is_mret && ~RegNext(io_hazard.is_mret)
 
     //delay
     lazy val lfsr = RegInit(IFU_DELAY)
@@ -107,7 +107,7 @@ class IFU extends Module {
     dontTouch(pc_next)
     
     val reg_pc = withReset(reset.asAsyncReset){
-        RegEnable(pc_next, START_ADDR, io_pipe.in.valid & io_pipe.in.ready)
+        RegEnable(pc_next, START_ADDR, io_pipe.in.valid)
     }
 
     val pc_plus4 = reg_pc + 4.U(WORD_LEN.W)
