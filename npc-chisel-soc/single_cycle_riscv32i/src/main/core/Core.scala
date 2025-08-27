8919c09483afa77752a0498544877519a3c00b32
tracer-ysyx <tracer@ysyx.org>
2025-08-27 16:33:47 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:47 up 1 day, 17:27,  4 users,  load average: 0.49, 0.92, 1.08

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -277,7 +277,7 @@ class Core extends Module {
         sel_jmp  -> exu.io.alu_out,
         sel_mret -> csr.io.csr_mepc
     ))
-    val pc_real_next = Mux(is_irq_r, csr.io.csr_mtvec, Mux(is_ctrl_hazard_r, pc_next_normal, ifu.io_hazard.pc_plus4))
+    val pc_real_next = Mux(is_irq, csr.io.csr_mtvec, Mux(is_ctrl_hazard, pc_next_normal, ifu.io_hazard.pc_plus4))
     ifu.io_hazard.pc_real_next := pc_real_next
 
 
