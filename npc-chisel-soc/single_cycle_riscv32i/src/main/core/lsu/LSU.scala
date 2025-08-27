7b6f8340fa8fbfd24c4fab8cd82d17dd64a14d00
tracer-ysyx <tracer@ysyx.org>
2025-08-27 21:47:49 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:49 up 1 day, 22:41,  3 users,  load average: 0.97, 0.95, 1.46

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/lsu/LSU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/lsu/LSU.scala
@@ -128,7 +128,7 @@ class LSU extends Module {
     ))
 
 
-    out_valid := Mux(n_state === s_AfterPreFire && fetch_normal)
+    out_valid := Mux(n_state === s_AfterPreFire && fetch_normal, true.B, false.B)
 
     switch(n_state){//third phase
         is(s_BeforePreFire){
