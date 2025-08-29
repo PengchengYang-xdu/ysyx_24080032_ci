4291a8bde2a2d36f0d0b936595c69717d6e99d64
tracer-ysyx <tracer@ysyx.org>
2025-08-29 17:04:49 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:04:49 up 3 days, 17:58,  3 users,  load average: 0.70, 0.99, 1.11

--- npc-chisel-soc/single_cycle_riscv32i/build/obj_dir/log.d
+++
@@ -1,5 +0,0 @@
-log.o: \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/csrc/utils/log.cpp \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/common.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/utils.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/include/debug.h
