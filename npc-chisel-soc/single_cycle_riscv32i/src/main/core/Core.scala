888c25196d53f00dec0f01b3a20d9801425d624c
tracer-ysyx <tracer@ysyx.org>
2025-08-30 18:16:49 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:16:49 up 4 days, 19:10,  3 users,  load average: 2.60, 1.99, 1.59

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -236,7 +236,7 @@ class Core extends Module {
     exu.io_pipe.in.bits.id2exe_rs1_data := RegEnable(Mux(rd1_forward_en || ~rawing, rd1_forward_data, rd1_forward_data_r), idu.io_pipe.out.fire)
     exu.io_pipe.in.bits.id2exe_rs2_data := RegEnable(Mux(rd2_forward_en || ~rawing, rd2_forward_data, rd2_forward_data_r), idu.io_pipe.out.fire)
 
-    idu.io_hazard.stall_flg := rawing
+    idu.io_hazard.stall_flg := is_raw || rawing
 
 
 
