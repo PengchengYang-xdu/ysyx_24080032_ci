320af92ccdd85d34f11861a0301b4f0e6f6144a2
tracer-ysyx <tracer@ysyx.org>
2025-08-30 16:20:04 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:04 up 4 days, 17:13,  3 users,  load average: 1.26, 1.99, 2.10

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -174,6 +174,9 @@ class Core extends Module {
     dontTouch(lsu_forward_data)
     dontTouch(wbu_forward_data)
 
+    val rd1_forward_data_r = RegEnable(rd1_forward_data, rd1_forward_en)
+    val rd2_forward_data_r = RegEnable(rd2_forward_data, rd2_forward_en)
+
     val rd1_forward_en = Wire(Bool())
     when(exu_raw_rs1){
         rd1_forward_en := exu_can_forward_rs1
@@ -208,10 +211,8 @@ class Core extends Module {
     dontTouch(rd2_forward_en)
     dontTouch(rd1_forward_data)
     dontTouch(rd2_forward_data)
-    exu.io_pipe.in.bits.id2exe_rs1_data := RegEnable(rd1_forward_data, idu.io_pipe.out.valid)
-    exu.io_pipe.in.bits.id2exe_rs2_data := RegEnable(rd2_forward_data, idu.io_pipe.out.valid)
-
-
+    exu.io_pipe.in.bits.id2exe_rs1_data := RegEnable(Mux(rd1_forward_en || ~is_raw, rd1_forward_data, rd1_forward_data_r), idu.io_pipe.out.fire)
+    exu.io_pipe.in.bits.id2exe_rs2_data := RegEnable(Mux(rd2_forward_en || ~is_raw, rd2_forward_data, rd2_forward_data_r), idu.io_pipe.out.fire)
 
     idu.io_hazard.stall_flg := is_raw && ~rd1_forward_en && ~rd2_forward_en
 
