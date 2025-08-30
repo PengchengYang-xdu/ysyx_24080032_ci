bb99e304e9df576e6d0a673418fed80ee3d613b0
tracer-ysyx <tracer@ysyx.org>
2025-08-30 17:40:26 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:40:26 up 4 days, 18:33,  3 users,  load average: 1.31, 1.27, 1.22

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -186,33 +186,33 @@ class Core extends Module {
 
     val rd1_forward_en = Wire(Bool())
     when(exu_raw_rs1_ing){
-        rd1_forward_en := exu_can_forward_rs1
+        rd1_forward_en := exu_can_forward_rs1 && exu_raw_rs1_ing
     }.elsewhen(lsu_raw_rs1_ing){
-        rd1_forward_en := lsu_can_forward_rs1
+        rd1_forward_en := lsu_can_forward_rs1 && lsu_raw_rs1_ing
     }.elsewhen(wbu_raw_rs1_ing){
-        rd1_forward_en := wbu_can_forward_rs1
+        rd1_forward_en := wbu_can_forward_rs1 && wbu_raw_rs1_ing
     }.otherwise{
         rd1_forward_en := false.B
     }
     val rd2_forward_en = Wire(Bool())
     when(exu_raw_rs2_ing){
-        rd2_forward_en := exu_can_forward_rs2
+        rd2_forward_en := exu_can_forward_rs2 && exu_raw_rs2_ing
     }.elsewhen(lsu_raw_rs2_ing){
-        rd2_forward_en := lsu_can_forward_rs2
+        rd2_forward_en := lsu_can_forward_rs2 && lsu_raw_rs2_ing
     }.elsewhen(wbu_raw_rs2_ing){
-        rd2_forward_en := wbu_can_forward_rs2
+        rd2_forward_en := wbu_can_forward_rs2 && wbu_raw_rs2_ing
     }.otherwise{
         rd2_forward_en := false.B
     }
     val rd1_forward_data = Mux(rd1_forward_en, MuxCase(0.U, Seq(
-        exu_can_forward_rs1 -> exu_forward_data,
-        lsu_can_forward_rs1 -> lsu_forward_data,
-        wbu_can_forward_rs1 -> wbu_forward_data
+        exu_can_forward_rs1 && exu_raw_rs1_ing -> exu_forward_data,
+        lsu_can_forward_rs1 && lsu_raw_rs1_ing -> lsu_forward_data,
+        wbu_can_forward_rs1 && wbu_raw_rs1_ing -> wbu_forward_data
     )), idu.io_pipe.out.bits.id2exe_rs1_data)
     val rd2_forward_data = Mux(rd2_forward_en, MuxCase(0.U, Seq(
-        exu_can_forward_rs2 -> exu_forward_data,
-        lsu_can_forward_rs2 -> lsu_forward_data,
-        wbu_can_forward_rs2 -> wbu_forward_data
+        exu_can_forward_rs2 && exu_raw_rs2_ing -> exu_forward_data,
+        lsu_can_forward_rs2 && lsu_raw_rs2_ing -> lsu_forward_data,
+        wbu_can_forward_rs2 && wbu_raw_rs2_ing -> wbu_forward_data
     )), idu.io_pipe.out.bits.id2exe_rs2_data)
     dontTouch(rd1_forward_en)
     dontTouch(rd2_forward_en)
