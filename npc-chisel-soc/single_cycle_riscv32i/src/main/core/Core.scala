40122481e1c5e578e62844cb7971a97803e6eaf0
tracer-ysyx <tracer@ysyx.org>
2025-08-31 15:56:02 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:56:02 up 5 days, 16:49,  3 users,  load average: 1.46, 1.94, 2.39

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -244,8 +244,8 @@ class Core extends Module {
     dontTouch(rd1_forward_data_r)
     dontTouch(rd2_forward_data_r)
 
-    exu.io_pipe.in.bits.id2exe_rs1_data := RegEnable(Mux(rd1_forward_en || ~rs1_rawing, rd1_forward_data, rd1_forward_data_r), idu.io_pipe.out.fire)
-    exu.io_pipe.in.bits.id2exe_rs2_data := RegEnable(Mux(rd2_forward_en || ~rs2_rawing, rd2_forward_data, rd2_forward_data_r), idu.io_pipe.out.fire)
+    exu.io_pipe.in.bits.id2exe_rs1_data := RegEnable(Mux(rd1_forward_en || (~rs1_rawing && ~bt_fwd_fsh), rd1_forward_data, rd1_forward_data_r), idu.io_pipe.out.fire)
+    exu.io_pipe.in.bits.id2exe_rs2_data := RegEnable(Mux(rd2_forward_en || (~rs2_rawing && ~bt_fwd_fsh), rd2_forward_data, rd2_forward_data_r), idu.io_pipe.out.fire)
     idu.io_hazard.stall_flg := (is_raw || rawing) && (~rd1_forward_en && ~rd2_forward_en && ~bt_fwd_fsh)
 
 
