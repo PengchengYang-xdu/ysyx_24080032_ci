224c5ac94c0e539be5b86a30d21e462e175bba39
tracer-ysyx <tracer@ysyx.org>
2025-08-31 22:10:48 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:10:48 up 5 days, 23:04,  4 users,  load average: 3.61, 3.64, 3.17

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -119,17 +119,11 @@ class Core extends Module {
     //data hazard
     val raw_rs1_cnt = RegInit(false.B)
     val raw_rs2_cnt = RegInit(false.B)
-    dontTouch(raw_rs1_cnt)
-    dontTouch(raw_rs2_cnt)
 
     val exu_is_working = ~exu.io_pipe.in.ready | exu.io_pipe.in.valid
     val lsu_is_working = ~lsu.io_pipe.in.ready | lsu.io_pipe.in.valid
     val wbu_is_working = ~wbu.io_pipe.in.ready | wbu.io_pipe.in.valid
     val wbu_end_flg = RegNext(wbu_is_working) & ~wbu_is_working
-    dontTouch(exu_is_working)
-    dontTouch(lsu_is_working)
-    dontTouch(wbu_is_working)
-    dontTouch(wbu_end_flg)
     val exu_raw = dataConflictWithStage(idu, exu_is_working, exu.io_pipe.in.bits.id2exe_wb_addr, exu.io_pipe.in.bits.id2exe_rf_wen === REN_S)
     val lsu_raw = dataConflictWithStage(idu, lsu_is_working, lsu.io_pipe.in.bits.exe2ls_wb_addr, lsu.io_pipe.in.bits.exe2ls_rf_wen === REN_S)
     val wbu_raw = dataConflictWithStage(idu, wbu_is_working, wbu.io_pipe.in.bits.ls2wb_wb_addr, wbu.io_pipe.in.bits.ls2wb_rf_wen === REN_S)
@@ -142,18 +136,6 @@ class Core extends Module {
     val wbu_raw_rs2 = wbu_raw && dataConflict(idu.io.gpr_rs2_addr, wbu.io_pipe.in.bits.ls2wb_wb_addr)
     val rs1_raw = exu_raw_rs1 || lsu_raw_rs1 || wbu_raw_rs1
     val rs2_raw = exu_raw_rs2 || lsu_raw_rs2 || wbu_raw_rs2
-    dontTouch(exu_raw)
-    dontTouch(lsu_raw)
-    dontTouch(wbu_raw)
-    dontTouch(is_raw)
-    dontTouch(exu_raw_rs1)
-    dontTouch(exu_raw_rs2)
-    dontTouch(lsu_raw_rs1)
-    dontTouch(lsu_raw_rs2)
-    dontTouch(wbu_raw_rs1)
-    dontTouch(wbu_raw_rs2)
-    dontTouch(rs1_raw)
-    dontTouch(rs2_raw)
 
     val rd1_forward_en = Wire(Bool())
     val rd2_forward_en = Wire(Bool())
@@ -174,24 +156,12 @@ class Core extends Module {
     val rs1_rawing = exu_rs1_rawing || lsu_rs1_rawing || wbu_rs1_rawing
     val rs2_rawing = exu_rs2_rawing || lsu_rs2_rawing || wbu_rs2_rawing
     val rawing = rs1_rawing || rs2_rawing
-    dontTouch(exu_rs1_rawing)
-    dontTouch(exu_rs2_rawing)
-    dontTouch(lsu_rs1_rawing)
-    dontTouch(lsu_rs2_rawing)
-    dontTouch(wbu_rs1_rawing)
-    dontTouch(wbu_rs2_rawing)
-    dontTouch(rs1_rawing)
-    dontTouch(rs2_rawing)
-    dontTouch(rawing)
 
     val bt_fwd_fsh_rs1 = RegInit(false.B)
     bt_fwd_fsh_rs1 := Mux(idu.io_pipe.out.fire, false.B, Mux(rd1_forward_en, true.B, bt_fwd_fsh_rs1))
     val bt_fwd_fsh_rs2 = RegInit(false.B)
     bt_fwd_fsh_rs2 := Mux(idu.io_pipe.out.fire, false.B, Mux(rd2_forward_en, true.B, bt_fwd_fsh_rs2))
     val bt_fwd_fsh = bt_fwd_fsh_rs1 | bt_fwd_fsh_rs2
-    dontTouch(bt_fwd_fsh_rs1)
-    dontTouch(bt_fwd_fsh_rs2)
-    dontTouch(bt_fwd_fsh)
 
 
     val exu_can_forward_rs1 = (exu_rs1_rawing || exu_raw_rs1) && (exu.io_pipe.in.bits.id2exe_wb_sel =/= WB_MEM || (lsu.io_pipe.out.fire && lsu.io_pipe.out.bits.ls2wb_wb_addr === idu.io.gpr_rs1_addr))
@@ -208,15 +178,6 @@ class Core extends Module {
     ))
     val lsu_forward_data = lsu.io_pipe.out.bits.ls2wb_wb_data
     val wbu_forward_data = wbu.io.gpr_wdata
-    dontTouch(exu_can_forward_rs1)
-    dontTouch(exu_can_forward_rs2)
-    dontTouch(lsu_can_forward_rs1)
-    dontTouch(lsu_can_forward_rs2)
-    dontTouch(wbu_can_forward_rs1)
-    dontTouch(wbu_can_forward_rs2)
-    dontTouch(exu_forward_data)
-    dontTouch(lsu_forward_data)
-    dontTouch(wbu_forward_data)
 
     when(exu_rs1_rawing || exu_raw_rs1){
         rd1_forward_en := exu_can_forward_rs1 && ~bt_fwd_fsh_rs1
@@ -248,12 +209,6 @@ class Core extends Module {
     )), idu.io_pipe.out.bits.id2exe_rs2_data)
     val rd1_forward_data_r = RegEnable(rd1_forward_data, rd1_forward_en)
     val rd2_forward_data_r = RegEnable(rd2_forward_data, rd2_forward_en)
-    dontTouch(rd1_forward_en)
-    dontTouch(rd2_forward_en)
-    dontTouch(rd1_forward_data)
-    dontTouch(rd2_forward_data)
-    dontTouch(rd1_forward_data_r)
-    dontTouch(rd2_forward_data_r)
 
     raw_rs1_cnt := Mux(rs1_raw && ~rd1_forward_en, true.B, Mux(rd1_forward_en, false.B, raw_rs1_cnt))
     raw_rs2_cnt := Mux(rs2_raw && ~rd2_forward_en, true.B, Mux(rd2_forward_en, false.B, raw_rs2_cnt))
