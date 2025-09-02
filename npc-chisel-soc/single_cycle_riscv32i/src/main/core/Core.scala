ba337fc77c7b21d7eb0621fe85e662a271c779b0
tracer-ysyx <tracer@ysyx.org>
2025-09-02 14:03:56 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:56 up 7 days, 14:57,  0 users,  load average: 4.07, 3.04, 2.42

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -41,10 +41,10 @@ class Core extends Module {
     val ready_r = RegNext(ifu.io_pipe.in.ready)
     ifu.io_pipe.in.valid := RegEnable(true.B, ifu.io_pipe.in.valid, ifu.io_pipe.in.ready & ready_r)
 
-    // pipelineConnect(ifu.io_pipe.out, idu.io_pipe.in)
-    // pipelineConnect(idu.io_pipe.out, exu.io_pipe.in)
-    // pipelineConnect(exu.io_pipe.out, lsu.io_pipe.in)
-    // pipelineConnect(lsu.io_pipe.out, wbu.io_pipe.in)
+    pipelineConnect(ifu.io_pipe.out, idu.io_pipe.in)
+    pipelineConnect(idu.io_pipe.out, exu.io_pipe.in)
+    pipelineConnect(exu.io_pipe.out, lsu.io_pipe.in)
+    pipelineConnect(lsu.io_pipe.out, wbu.io_pipe.in)
 
 
 
