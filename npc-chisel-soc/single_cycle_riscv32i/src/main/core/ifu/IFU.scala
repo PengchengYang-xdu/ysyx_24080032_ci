35aeea2719286cd2d8a4e5136718ce21fc126a6d
tracer-ysyx <tracer@ysyx.org>
2025-09-02 16:36:42 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:42 up 7 days, 17:30,  0 users,  load average: 2.34, 2.44, 2.61

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/ifu/IFU.scala
@@ -12,6 +12,7 @@ class IFUIO_HAZARD extends Bundle {
     val pc_plus4 = Output(UInt(WORD_LEN.W))
     val pc_real_next = Input(UInt(WORD_LEN.W))
     val is_mret_rise = Input(Bool())
+    val reg_pc = Output(UInt(WORD_LEN.W))
 }
 
 
@@ -168,6 +169,7 @@ class IFU extends Module {
     
     //connect
     araddr := reg_pc
+    io_hazard.reg_pc := reg_pc
 
     io_pipe.out.bits.if2id_reg_pc := reg_pc
     io_pipe.out.bits.if2id_inst := io.imem.rdata
