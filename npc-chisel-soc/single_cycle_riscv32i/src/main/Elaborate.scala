43cca2b7bfab0f6204995ccb4e139af33101c378
tracer-ysyx <tracer@ysyx.org>
2025-09-05 17:53:26 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:26 up 10 days, 18:46,  0 users,  load average: 1.81, 1.90, 1.67

--- npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
@@ -10,7 +10,7 @@ import npc.core.wbu._
 import npc.perip._
 
 object Elaborate extends App {
-  // === 单独测试 instList ===
+    // === 单独测试 instList ===
   println("=== Instruction List ===")
   instList.foreach { insn =>
     println(s"${insn.inst.toString}")
@@ -19,15 +19,15 @@ object Elaborate extends App {
 
 
 
-  
-  val firtoolOptions = Array("--lowering-options=" + List(
-    // make yosys happy
-    // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-    "disallowLocalVariables",
-    "disallowPackedArrays",
-    "locationInfoStyle=wrapInAtSquareBracket"
-  ).reduce(_ + "," + _))
-  // circt.stage.ChiselStage.emitSystemVerilogFile(new npc.core.Core, args, firtoolOptions)
-  circt.stage.ChiselStage.emitSystemVerilogFile(new npc.NPC, args, firtoolOptions)
-  // circt.stage.ChiselStage.emitSystemVerilogFile(new npc.core.ifu.IFU, args, firtoolOptions)
+
+//   val firtoolOptions = Array("--lowering-options=" + List(
+//     // make yosys happy
+//     // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
+//     "disallowLocalVariables",
+//     "disallowPackedArrays",
+//     "locationInfoStyle=wrapInAtSquareBracket"
+//   ).reduce(_ + "," + _))
+//   // circt.stage.ChiselStage.emitSystemVerilogFile(new npc.core.Core, args, firtoolOptions)
+//   circt.stage.ChiselStage.emitSystemVerilogFile(new npc.NPC, args, firtoolOptions)
+//   // circt.stage.ChiselStage.emitSystemVerilogFile(new npc.core.ifu.IFU, args, firtoolOptions)
 }
--- npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
@@ -68,33 +68,34 @@ object Instructions {
 }
 
 
+object InstructionNew {
+    val instTable: Iterable[rvdecoderdb.Instruction] = rvdecoderdb.instructions(os.pwd / "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
 
-val instTable: Iterable[rvdecoderdb.Instruction] = rvdecoderdb.instructions(os.pwd / "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
+    case class InstructionPattern (
+      val inst: rvdecoderdb.Instruction
+    ) extends DecodePattern {
+      override def bitPat: BitPat = BitPat("b" + inst.encoding.toString())
+    }
 
-case class InstructionPattern (
-  val inst: rvdecoderdb.Instruction
-) extends DecodePattern {
-  override def bitPat: BitPat = BitPat("b" + inst.encoding.toString())
+    object InstructionPattern {
+      implicit class addMethodsToInsn(i: InstructionPattern) {
+        def hasArg(arg: String) = i.inst.args.map(_.name).contains(arg)
+      }
+    }
+
+    val rv32iExceptionInstructions = 
+        Set("sbreak", "scall", "pause", "fence.tso", "fence", "slli_rv32", "srli_rv32", "srai_rv32")
+    val rv32iTargetSets = Set("rv_i", "rv32_i")
+    val rvzicsrTargetSets = Set("rv_zicsr")
+    val rv32iInstList = instTable
+        .filter(instr => rv32iTargetSets.contains(instr.instructionSet.name))
+        .filter(instr => !rv32iExceptionInstructions.contains(instr.instructionSet.name))
+        .map(InstructionPattern(_))
+        .toSeq
+    val rvzicsrInstList = instTable
+        .filter(instr => rvzicsrTargetSets.contains(instr.instructionSet.name))
+        .filter(_.pseudoFrom.isEmpty)
+        .map(InstructionPattern(_))
+        .toSeq
+    val instList = rv32iInstList ++ rvzicsrInstList
 }
-
-object InstructionPattern {
-  implicit class addMethodsToInsn(i: InstructionPattern) {
-    def hasArg(arg: String) = i.inst.args.map(_.name).contains(arg)
-  }
-}
-
-val rv32iExceptionInstructions = 
-    Set("sbreak", "scall", "pause", "fence.tso", "fence", "slli_rv32", "srli_rv32", "srai_rv32")
-val rv32iTargetSets = Set("rv_i", "rv32_i")
-val rvzicsrTargetSets = Set("rv_zicsr")
-val rv32iInstList = instTable
-    .filter(instr => rv32iTargetSets.contains(instr.instructionSet.name))
-    .filter(instr => !rv32iExceptionInstructions.contains(instr.instructionSet.name))
-    .map(InstructionPattern(_))
-    .toSeq
-val rvzicsrInstList = instTable
-    .filter(instr => rvzicsrTargetSets.contains(instr.instructionSet.name))
-    .filter(_.pseudoFrom.isEmpty)
-    .map(InstructionPattern(_))
-    .toSeq
-val instList = rv32iInstList ++ rvzicsrInstList
