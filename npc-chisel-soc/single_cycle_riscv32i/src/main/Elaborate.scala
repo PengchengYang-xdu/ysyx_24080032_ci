5625d46c5e6907334ca4a84192fdfb0e247721a0
tracer-ysyx <tracer@ysyx.org>
2025-09-06 10:00:23 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:00:23 up 11 days, 10:53,  0 users,  load average: 3.60, 3.42, 2.92

--- npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
@@ -12,8 +12,8 @@ import npc.perip._
 object Elaborate extends App {
     // === 单独测试 instList ===
   println("=== Instruction List ===")
-  allPossibleInput = allPossibleInput(os.pwd / "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
-  allPossibleInput.map(_.toString).foreach(println)
+  instInput = allPossibleInput(os.pwd / "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
+  instInput.map(_.toString).foreach(println)
 
 
 
