fd6c12d882ff172eeeb735164b800552d4dabcbf
tracer-ysyx <tracer@ysyx.org>
2025-09-06 10:03:13 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:13 up 11 days, 10:56,  0 users,  load average: 3.98, 3.81, 3.16

--- npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
@@ -6,7 +6,8 @@ import chisel3.util.experimental.decode._
 import org.chipsalliance.rvdecoderdb
 
 
-object Instructions{
+object Instructions {
+    val instTable: Iterable[rvdecoderdb.Instruction] = rvdecoderdb.instructions(os.pwd / "rvdecoderdb" / "rvdecoderdbtest" / "jvm" / "riscv-opcodes")
 
     case class InstructionPattern (
       val inst: rvdecoderdb.Instruction
@@ -20,22 +21,18 @@ object Instructions{
       }
     }
 
-    object allPossibleInput {
-        def apply(opcodePath: os.Path): Seq[InstructionPattern] = {
-            val instTable: Iterable[rvdecoderdb.Instruction] = rvdecoderdb.instructions(opcodePath)
-            val instSets = Set("rv_i", "rv_zicsr", "rv_system", "rv_zifencei")
-            val ex_rv_i = Set("fence")
-            val ex_rv_zicsr = Set("csrrc", "csrrwi", "csrrsi", "csrrci")
-            val ex_rv_system = Set("wfi")
-            val instList = instTable
-              .filter(_.pseudoFrom.isEmpty) //伪指令不考虑
-              .filter(inst => instSets.contains(inst.instructionSet.name))
-              .filter(inst => !ex_rv_i.contains(inst.name))
-              .filter(inst => !ex_rv_zicsr.contains(inst.name))
-              .filter(inst => !ex_rv_system.contains(inst.name))
-              .map(InstructionPattern(_))
-              .toSeq
-            instList
-        }
-    }
+
+    val instSets = Set("rv_i", "rv_zicsr", "rv_system", "rv_zifencei")
+    val ex_rv_i = Set("fence")
+    val ex_rv_zicsr = Set("csrrc", "csrrwi", "csrrsi", "csrrci")
+    val ex_rv_system = Set("wfi")
+    val patternSeq = instTable
+      .filter(_.pseudoFrom.isEmpty) //伪指令不考虑
+      .filter(inst => instSets.contains(inst.instructionSet.name))
+      .filter(inst => !ex_rv_i.contains(inst.name))
+      .filter(inst => !ex_rv_zicsr.contains(inst.name))
+      .filter(inst => !ex_rv_system.contains(inst.name))
+      .map(InstructionPattern(_))
+      .toSeq
+    val instList = patternSeq
 }
