060e37e2ccc8c6ec94b230dcee571f1bb7e2d8b8
tracer-ysyx <tracer@ysyx.org>
2025-09-15 17:03:52 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:52 up 20 days, 17:57,  0 users,  load average: 1.94, 2.90, 2.18

--- npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/common/Instructions.scala
@@ -85,12 +85,12 @@ object Instructions{
 
     object ProcessUnit extends DecodeField[Insn, UInt] with InstCateg{
         def name = "processunit"
-        def chiselType = UInt(ProcessUnit.ProcessUnit_Width.W)
+        def chiselType = UInt(ProcessUnit_Width.W)
         def genTable(i: Insn): BitPat = i.inst.name match {
-            case name if aluInst.contains(name) || bjInst.contains(name) => BitPat(ProcessUnit.ALU)
-            case name if csrInst.contains(name) => BitPat(ProcessUnit.CSR)
-            case name if lsuInst.contains(name) => BitPat(ProcessUnit.LSU)
-            case _ => BitPat(ProcessUnit.ALU)
+            case name if aluInst.contains(name) || bjInst.contains(name) => BitPat(ALU)
+            case name if csrInst.contains(name) => BitPat(CSR)
+            case name if lsuInst.contains(name) => BitPat(LSU)
+            case _ => BitPat(ALU)
         }
     }
 
