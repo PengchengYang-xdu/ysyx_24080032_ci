d810ada90f78fa4778f24ee618a70aaf566eb6f9
tracer-ysyx <tracer@ysyx.org>
2025-09-16 16:03:33 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:33 up 21 days, 16:56,  0 users,  load average: 2.00, 1.85, 1.73

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/wbu/WBU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/wbu/WBU.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 import npc.common.Config._
 import npc.common._
 import npc.common.Instructions._
-import npc.core.exu
+import npc.core.exu._
 /*
               ___ _____ _   _ _____ ____  ____ ___ ____
              / _ \_   _| | | | ____|  _ \/ ___|_ _/ ___|
