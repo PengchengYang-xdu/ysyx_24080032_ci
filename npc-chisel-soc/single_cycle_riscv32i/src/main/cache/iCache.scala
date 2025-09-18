69e27ec2693ef1198568eee444a09853710b92c5
tracer-ysyx <tracer@ysyx.org>
2025-09-18 17:25:44 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:25:44 up 23 days, 18:19,  0 users,  load average: 2.56, 2.29, 2.47

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -107,9 +107,12 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
     val req_tag = Wire(UInt(tag_width.W))
     req_tag := io.in.araddr(31, m + n)
 
-    val req_index_r = Mux(n_state === s_icache_lookup, req_index, Mux(n_state === s_IDLE, 0.U, req_index_r))
-    val req_tag_r = Mux(n_state === s_icache_lookup, req_tag, Mux(n_state === s_IDLE, 0.U, req_tag_r))
-    val req_offset_r = Mux(n_state === s_icache_lookup, req_offset, Mux(n_state === s_IDLE, 0.U, req_offset_r))
+    val req_index_r = RegInit(0.U(index_width.W))
+    req_index_r := Mux(n_state === s_icache_lookup, req_index, Mux(n_state === s_IDLE, 0.U, req_index_r))
+    val req_tag_r = RegInit(0.U(tag_width.W))
+    req_tag_r:= Mux(n_state === s_icache_lookup, req_tag, Mux(n_state === s_IDLE, 0.U, req_tag_r))
+    val req_offset_r = RegInit(0.U(offset_width.W))
+    req_offset_r := Mux(n_state === s_icache_lookup, req_offset, Mux(n_state === s_IDLE, 0.U, req_offset_r))
 
     val addr_align = Wire(UInt(WORD_LEN.W))
     addr_align := io.in.araddr - req_offset_r
@@ -130,7 +133,8 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int, val replacementP
     val issdram_raddr = (io.in.araddr >= "ha000_0000".U(32.W) && io.in.araddr <= "hbfff_ffff".U(32.W))
     val isifu_rreq = io.in.arvalid & in_arready
 
-    val issdram_raddr_r = Mux(n_state === s_icache_lookup, issdram_raddr, Mux(n_state === s_IDLE, false.B, issdram_raddr_r))
+    issdram_raddr_r = RegInit(false.B)
+    issdram_raddr_r := Mux(n_state === s_icache_lookup, issdram_raddr, Mux(n_state === s_IDLE, false.B, issdram_raddr_r))
 
 
     val ways_hit = Wire(Bool())
