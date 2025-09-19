83b74866e7feed8fda3898369220654cc71ea16a
tracer-ysyx <tracer@ysyx.org>
2025-09-19 21:43:20 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:20 up 24 days, 22:36,  0 users,  load average: 2.82, 2.23, 2.82

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -82,6 +82,7 @@ class Core extends Module {
 
     idu.io_flush.flush_flg := ifu.io_flush.flush_flg
     isu.io_flush.flush_flg := ifu.io_flush.flush_flg
+    icache.io_flush.flush_flg := ifu.io_flush.flush_flg
 
 
 
