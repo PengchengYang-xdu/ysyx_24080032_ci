6ca998a4e6a79b4f9f600bc03f793705885df4e2
tracer-ysyx <tracer@ysyx.org>
2025-09-21 09:13:48 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:13:48 up 26 days, 10:07,  0 users,  load average: 0.86, 1.21, 1.48

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -37,7 +37,7 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int) extends Module{
     val req_index = io.in.araddr(m + n - 1, m)
     val req_offset = io.in.araddr(m - 1, 0)
     val req_tag = io.in.araddr(WORD_LEN, m + n)
-    val addr_align := io.in.araddr - req_offset
+    val addr_align = io.in.araddr - req_offset
     dontTouch(count)
     dontTouch(req_index)
     dontTouch(req_offset)
