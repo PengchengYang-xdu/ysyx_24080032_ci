7f93cac7f5665ec2ad71d4955c3446346d24a184
tracer-ysyx <tracer@ysyx.org>
2025-09-21 09:34:17 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:34:17 up 26 days, 10:27,  0 users,  load average: 2.75, 3.15, 2.73

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -57,7 +57,7 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int) extends Module{
     val is_sdram_raddr = (io.in.araddr >= "ha000_0000".U(WORD_LEN.W) && io.in.araddr <= "hbfff_ffff".U(WORD_LEN.W))
     val is_ifu_ar_req = io.in.arvalid//无需等待arready 先查询cache
     val is_ifu_ar_fire = io.in.arvalid && io.in.arready
-    val is_fetch_done = io.out.rvalid && io.out.rready
+    val is_fetch_done = io.in.rvalid && io.in.rready
 
     val hit = WireDefault(false.B)
     val hit_num = WireDefault(0.U(ways_width.W))
