dcac0986cc774025f75fa545c4ac04890a205a8e
tracer-ysyx <tracer@ysyx.org>
2025-09-24 20:30:18 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:18 up 29 days, 21:23,  0 users,  load average: 3.05, 2.42, 2.02

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/idu/IDU.scala
@@ -99,7 +99,7 @@ class IDU extends Module{
     io_pipe.out.bits.id2is_rd_addr := inst(11, 7)
     io_pipe.out.bits.id2is_ch1tpe := decodeBundle(MyCH1Tpe)
     io_pipe.out.bits.id2is_ch2tpe := decodeBundle(MyCH2Tpe)
-    io_pipe.out.bits.id2is_rs1_addr := Mux(opcode === "b0110111".U, 0.U, (19, 15))
+    io_pipe.out.bits.id2is_rs1_addr := Mux(opcode === "b0110111".U, 0.U, inst(19, 15))
     io_pipe.out.bits.id2is_rs2_addr := inst(24, 20)
     io_pipe.out.bits.id2is_reg_pc := reg_pc
     io_pipe.out.bits.id2is_imm := imm
