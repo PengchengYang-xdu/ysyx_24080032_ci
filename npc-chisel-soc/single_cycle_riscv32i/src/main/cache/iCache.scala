1bd28c0444ce6fd98890bf7900963327baa580e5
tracer-ysyx <tracer@ysyx.org>
2025-09-26 16:44:43 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:43 up 31 days, 17:38,  0 users,  load average: 2.60, 4.06, 3.97

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -166,13 +166,13 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int) extends Module{
         val set = icache(req_index).set
         when(hasEmpty === true.B) {
             // 如果有空闲块，填充
-            set(emptyIndex).valid := true.B
+            set(emptyIndex).valid := io.out.rlast//突发传输，分阶段写data块，valid只在最后一次拉高，否则最后一次的data块因为没有hasEmpty而写入失败
             set(emptyIndex).tag := req_tag
             set(emptyIndex).data(count) := io.out.rdata
         }.otherwise{
             // 如果没有空闲块，替换逻辑
             val randomIndex = scala.util.Random.nextInt(ways)
-            set(randomIndex).valid := true.B
+            set(randomIndex).valid := io.out.rlast
             set(randomIndex).tag := req_tag
             set(randomIndex).data(count) := io.out.rdata
         }
