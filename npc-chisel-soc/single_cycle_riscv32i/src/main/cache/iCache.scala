23518697eb51a44d2ac869634aaeb7660048d6f9
tracer-ysyx <tracer@ysyx.org>
2025-09-26 15:22:50 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:50 up 31 days, 16:16,  0 users,  load average: 3.14, 2.48, 2.38

--- npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/cache/iCache.scala
@@ -104,7 +104,7 @@ class iCache(val block_size: Int, val sets: Int, val ways: Int) extends Module{
     require(c != 0, "c must be greater than 0")
     val count = RegInit(c.U(log2Ceil(c + 1).W))
     when(is_imem_ar_fire){
-        count := c
+        count := c.U
     }.elsewhen(is_imem_r_fire){
         count := count - 1.U
     }
