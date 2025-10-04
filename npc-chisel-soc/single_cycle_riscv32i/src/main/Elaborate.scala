299a72a5b151ca3ec85481d72ae95a8ae854752e
tracer-ysyx <tracer@ysyx.org>
2025-10-04 14:06:45 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:45 up 39 days, 15:00,  2 users,  load average: 5.14, 5.27, 4.40

--- npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/Elaborate.scala
@@ -10,22 +10,25 @@ import npc.core.wbu._
 import npc.perip._
 
 object Elaborate extends App {
-    // // === 单独测试 instList ===
-    // println("=== Instruction List ===")
-    // instList.foreach { InstructionPattern =>
-    //   println(s"${InstructionPattern.toString}")
-    // }
 
-
-
-
-
-    val firtoolOptions = Array("--lowering-options=" + List(
+    // 1. 将 lowering-options 字符串单独计算出来
+    val loweringOptionsString = List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
       "disallowLocalVariables",
       "disallowPackedArrays",
       "locationInfoStyle=wrapInAtSquareBracket"
-    ).reduce(_ + "," + _))
+    ).reduce(_ + "," + _)
+
+    // 2. 定义你想使用的前缀
+    val modulePrefix = "ysyx_24080032_" // 将你的前缀定义在这里
+
+    // 3. 构造 firtoolOptions 数组
+    val firtoolOptions = Array(
+        s"--lowering-options=$loweringOptionsString", // 保持原有的 lowering-options
+        s"-prefix=$modulePrefix"                       // 添加 -prefix 选项
+    )
+
+    // 4. 调用 ChiselStage
     circt.stage.ChiselStage.emitSystemVerilogFile(new npc.NPC, args, firtoolOptions)
 }
