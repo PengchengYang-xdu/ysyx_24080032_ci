1e0cdaaa3488da06f14fca39786c5fe341b8f1d4
tracer-ysyx <tracer@ysyx.org>
2025-10-05 15:29:41 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:41 up 40 days, 16:23,  2 users,  load average: 3.51, 3.13, 3.09

--- npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/core/Core.scala
@@ -55,7 +55,10 @@ class Core extends Module {
     exu.io_pipe.in.bits.is2exe_ch1 := RegEnable(isu.io_pipe.out.bits.is2exe_ch1, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
     exu.io_pipe.in.bits.is2exe_ch2 := RegEnable(isu.io_pipe.out.bits.is2exe_ch2, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
     exu.io_pipe.in.bits.is2exe_ch3 := RegEnable(isu.io_pipe.out.bits.is2exe_ch3, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
-    exu.io_pipe.in.bits.diff := RegEnable(isu.io_pipe.out.bits.diff, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
+
+    exu.io_pipe.in.bits.diff.get.pc := RegEnable(isu.io_pipe.out.bits.diff.get.pc, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
+    exu.io_pipe.in.bits.diff.get.inst := RegEnable(isu.io_pipe.out.bits.diff.get.inst, isu.io_pipe.out.valid && exu.io_pipe.in.ready)
+
     exu.io_pipe.in.valid := RegEnable(isu.io_pipe.out.valid, exu.io_pipe.in.ready)
 
     pipelineConnect(exu.io_pipe.out, wbu.io_pipe.in)
