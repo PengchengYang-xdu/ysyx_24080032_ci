345a7b43534f2b96ea8c5cd462449b66d19ff66a
tracer-ysyx <tracer@ysyx.org>
2025-10-06 14:53:08 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:08 up 41 days, 15:46,  2 users,  load average: 5.27, 5.36, 5.20

--- npc-chisel-soc/single_cycle_riscv32i/iverilog/tb/tb_top.sv
+++ npc-chisel-soc/single_cycle_riscv32i/iverilog/tb/tb_top.sv
@@ -17,8 +17,8 @@ initial begin
 end
 
 initial begin
-	$dumpfile("/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/iverilog/wave/top.fst");
-	$dumpvars(0, tb_top);
+	// $dumpfile("/home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel-soc/single_cycle_riscv32i/iverilog/wave/top.fst");
+	// $dumpvars(0, tb_top);
 	// repeat (1000000) @(posedge clock);
 	// $display("TIMEOUT");
 	// $finish;
