90f4b7be90dbc2295f82ea29bfd5d13f98dc92e3
tracer-ysyx <tracer@ysyx.org>
2025-10-06 19:56:55 +0800

    > generate verilog ysyx_24080032 杨鹏城 Linux ubuntu-4029 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:56:55 up 41 days, 20:50,  2 users,  load average: 4.01, 3.64, 3.75

--- npc-chisel-soc/single_cycle_riscv32i/src/main/common/Config.scala
+++ npc-chisel-soc/single_cycle_riscv32i/src/main/common/Config.scala
@@ -11,7 +11,7 @@ object Config{
     val WORD_LEN = 32
     val BYTE_LEN = 8
 
-    // val START_ADDR = "h80000000".U(WORD_LEN.W) /*THIS IS NPC MODE*/
+    // val START_ADDR = "h30000000".U(WORD_LEN.W) /*THIS IS NPC MODE*/
     // val START_ADDR = "h20000000".U(WORD_LEN.W) /*THIS IS MROM MODE*/
     val START_ADDR = "h80000000".U(WORD_LEN.W) /*THIS IS FLASH MODE*/
 
@@ -19,7 +19,7 @@ object Config{
     val ADDR_LEN = 4 // rs1,rs2,wb
     val CSR_ADDR_LEN = 12
 
-    val DIFFTEST = true
+    val DIFFTEST = false
 
     val RENAME = true
 }
