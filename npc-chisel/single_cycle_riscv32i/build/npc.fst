e68c2c3c039198b0b80b89a28b003e37810a34fb
PengchengYang-xdu <24241214835@stu.xidian.edu.cn>
2025-10-08 15:22:48 +0800

    modify v2 of ci branch

--- npc-chisel/single_cycle_riscv32i/build/npc.fst
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4aa899a71a6f1a575d6165aa5cf09d471cb192b48a87d909e6f383b73c187f5d
-size 330
--- npc-chisel/single_cycle_riscv32i/build/obj_dir/monitor.d
+++
@@ -1,7 +0,0 @@
-monitor.o: \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/csrc/monitor/monitor.cpp \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/mem.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/common.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/device.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/utils.h \
- /home/yangpengcheng/ysyx/ysyx/ysyx-workbench/npc-chisel/single_cycle_riscv32i/include/debug.h
